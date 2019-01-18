5c741d1f4bf775ce6dd5bfac33e2e463bcef7d1d
Michael Woolnough <michael.woolnough@gmail.com>
2019-01-18 15:31:37 +0000

    copied from jslib/cmd

---
+++ tokeniser.go
@@ -0,0 +1,551 @@
+package javascript
+
+import (
+	"io"
+	"strings"
+	"unicode"
+
+	"vimagination.zapto.org/errors"
+	"vimagination.zapto.org/parser"
+)
+
+const (
+	whitespace      = "\t\v\f \xa0\ufeff" // Tab, Vertical Tab, Form Feed, Space, No-break space, ZeroWidth No-Break Space, https://www.ecma-international.org/ecma-262/8.0/index.html#table-32
+	lineTerminators = "\n\r\u2028\u2029"  // Line Feed, Carriage Return, Line Separator, Paragraph Separator, https://www.ecma-international.org/ecma-262/8.0/index.html#table-33
+
+	singleEscapeChar = "'\"\\bfnrtv"
+	binaryDigit      = "01"
+	octalDigit       = "01234567"
+	decimalDigit     = "0123456789"
+	hexDigit         = "0123456789abcdefABCDEF"
+
+	zwnj rune = 8204
+	zwj  rune = 8205
+)
+
+var keywords = [...]string{"await", "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"}
+
+const (
+	TokenWhitespace parser.TokenType = iota
+	TokenLineTerminator
+	TokenSingleLineComment
+	TokenMultiLineComment
+	TokenIdentifier
+	TokenBooleanLiteral
+	TokenKeyword
+	TokenPunctuator
+	TokenNumericLiteral
+	TokenStringLiteral
+	TokenNoSubstitutionTemplate
+	TokenTemplateHead
+	TokenTemplateMiddle
+	TokenTemplateTail
+	TokenDivPunctuator
+	TokenRightBracePunctuator
+	TokenRegularExpressionLiteral
+)
+
+var (
+	idContinue = []*unicode.RangeTable{
+		unicode.L,
+		unicode.Nl,
+		unicode.Other_ID_Start,
+		unicode.Mn,
+		unicode.Mc,
+		unicode.Nd,
+		unicode.Pc,
+		unicode.Other_ID_Continue,
+	}
+	idStart = idContinue[:3]
+	notID   = []*unicode.RangeTable{
+		unicode.Pattern_Syntax,
+		unicode.Pattern_White_Space,
+	}
+)
+
+func isIDStart(c rune) bool {
+	if c == '$' || c == '_' || c == '\\' {
+		return true
+	}
+	return unicode.In(c, idStart...) && !unicode.In(c, notID...)
+}
+
+func isIDContinue(c rune) bool {
+	if c == '$' || c == '_' || c == '\\' || c == zwnj || c == zwj {
+		return true
+	}
+	return unicode.In(c, idContinue...) && !unicode.In(c, notID...)
+}
+
+type jsParser struct {
+	tokenDepth      []byte
+	divisionAllowed bool
+}
+
+func (j *jsParser) inputElement(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	if t.Accept(whitespace) {
+		t.AcceptRun(whitespace)
+		return parser.Token{
+			Type: TokenWhitespace,
+			Data: t.Get(),
+		}, j.inputElement
+	}
+	if t.Accept(lineTerminators) {
+		t.AcceptRun(lineTerminators)
+		return parser.Token{
+			Type: TokenLineTerminator,
+			Data: t.Get(),
+		}, j.inputElement
+	}
+	allowDivision := j.divisionAllowed
+	j.divisionAllowed = false
+	c := t.Peek()
+	switch c {
+	case -1:
+		if len(j.tokenDepth) == 0 {
+			return t.Done()
+		}
+		t.Err = io.ErrUnexpectedEOF
+		return t.Error()
+	case '/':
+		t.Except("")
+		if t.Accept("/") {
+			t.ExceptRun(lineTerminators)
+			return parser.Token{
+				Type: TokenSingleLineComment,
+				Data: t.Get(),
+			}, j.inputElement
+		}
+		if t.Accept("*") {
+			t.ExceptRun("*")
+			t.Accept("*")
+			if t.Accept("/") {
+				j.divisionAllowed = allowDivision
+				return parser.Token{
+					Type: TokenMultiLineComment,
+					Data: t.Get(),
+				}, j.inputElement
+			}
+			if t.Peek() == -1 {
+				t.Err = io.ErrUnexpectedEOF
+			} else {
+				t.Except("")
+				t.Err = errors.WithContext("error parsing comment: ", errors.Error(t.Get()))
+			}
+			return t.Error()
+		}
+		if allowDivision {
+			t.Accept("=")
+			return parser.Token{
+				Type: TokenDivPunctuator,
+				Data: t.Get(),
+			}, j.inputElement
+		}
+		j.divisionAllowed = true
+		return j.regexp(t)
+	case '}':
+		t.Except("")
+		switch j.lastDepth() {
+		case '{':
+			j.tokenDepth = j.tokenDepth[:len(j.tokenDepth)-1]
+			return parser.Token{
+				Type: TokenRightBracePunctuator,
+				Data: t.Get(),
+			}, j.inputElement
+		case '$':
+			j.tokenDepth = j.tokenDepth[:len(j.tokenDepth)-1]
+			return j.template(t)
+		}
+		t.Err = errors.WithContext("invalid character: ", errors.Error(t.Get()))
+		return t.Error()
+	case '\'', '"':
+		j.divisionAllowed = true
+		return j.stringToken(t)
+	case '`':
+		t.Except("")
+		return j.template(t)
+	default:
+		if strings.ContainsRune(decimalDigit, c) {
+			j.divisionAllowed = true
+			return j.number(t)
+		}
+		if isIDStart(c) {
+			tk, tf := j.identifier(t)
+			if tk.Type == TokenIdentifier {
+				if tk.Data == "true" || tk.Data == "false" {
+					j.divisionAllowed = true
+					tk.Type = TokenBooleanLiteral
+				} else if tk.Data == "Infinity" {
+					j.divisionAllowed = true
+					tk.Type = TokenNumericLiteral
+				} else {
+					for _, kw := range keywords {
+						if kw == tk.Data {
+							tk.Type = TokenKeyword
+							if tk.Data == "this" {
+								j.divisionAllowed = true
+							}
+							break
+						}
+					}
+					if tk.Type == TokenIdentifier {
+						j.divisionAllowed = true
+					}
+				}
+			}
+			return tk, tf
+		}
+		t.Except("")
+		switch c {
+		case '{', '(', '[':
+			j.tokenDepth = append(j.tokenDepth, byte(c))
+		case ';', ',', '?', ':', '~':
+		case ')', ']':
+			if ld := j.lastDepth(); !(ld == '(' && c == ')') && !(ld == '[' && c == ']') {
+				t.Err = errors.WithContext("read invalid character: ", errors.Error(t.Get()))
+				return t.Error()
+			}
+			j.divisionAllowed = true
+			j.tokenDepth = j.tokenDepth[:len(j.tokenDepth)-1]
+		case '.':
+			if t.Accept(".") {
+				if !t.Accept(".") { // ...
+					if t.Peek() == -1 {
+						t.Err = io.ErrUnexpectedEOF
+					} else {
+						t.Except("")
+						t.Err = errors.WithContext("invalid character sequence: ", errors.Error(t.Get()))
+					}
+					return t.Error()
+				}
+			}
+		case '<', '>':
+			if !t.Accept("=") { //>=, <=
+				if t.Peek() == c { // >>, <<
+					t.Except("")
+					if !t.Accept("=") && c == '>' { // >>=, <<=
+						t.Accept(">") // >>>
+					}
+				}
+			}
+		case '=':
+			if t.Accept("=") { // ==
+				t.Accept("=") // ===
+			} else {
+				t.Accept(">") // =>
+			}
+		case '!':
+			if t.Accept("=") { // !=
+				t.Accept("=") // !==
+			}
+		case '+', '-':
+			if t.Peek() == c {
+				t.Except("") // ++, --
+			} else {
+				t.Accept("=") // +=, -=
+			}
+		case '*':
+			t.Accept("*=") // **, *=
+		case '&', '|':
+			if t.Peek() == c {
+				t.Except("") // &&, ||
+			} else {
+				t.Accept("=") // &=, |=
+			}
+		case '%', '^':
+			t.Accept("=") // %=, ^=
+		default:
+			t.Err = errors.WithContext("read invalid character: ", errors.Error(t.Get()))
+			return t.Error()
+		}
+		return parser.Token{
+			Type: TokenPunctuator,
+			Data: t.Get(),
+		}, j.inputElement
+	}
+}
+
+func (j *jsParser) regexpBackslashSequence(t *parser.Tokeniser) bool {
+	t.Except("")
+	if !t.Except(lineTerminators) {
+		if t.Peek() == -1 {
+			t.Err = io.ErrUnexpectedEOF
+		} else {
+			t.Except("")
+			t.Err = errors.WithContext("invalid regexp character: ", errors.Error(t.Get()))
+		}
+		return false
+	}
+	return true
+}
+
+func (j *jsParser) regexpExpressionClass(t *parser.Tokeniser) bool {
+	t.Except("")
+	for {
+		switch t.ExceptRun("]\\") {
+		case ']':
+			t.Except("")
+			return true
+		case '\\':
+			if !j.regexpBackslashSequence(t) {
+				return false
+			}
+		default:
+			t.Err = io.ErrUnexpectedEOF
+			return false
+		}
+	}
+}
+
+func (j *jsParser) regexp(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	switch c := t.Peek(); c {
+	case -1:
+		t.Err = io.ErrUnexpectedEOF
+		return t.Error()
+	case '\\':
+		if !j.regexpBackslashSequence(t) {
+			return t.Error()
+		}
+	case '[':
+		if !j.regexpExpressionClass(t) {
+			return t.Error()
+		}
+	default:
+		t.Except("")
+		if strings.ContainsRune(lineTerminators, c) {
+			t.Err = errors.WithContext("invalid regexp character: ", errors.Error(t.Get()))
+			return t.Error()
+		}
+	}
+Loop:
+	for {
+		switch c := t.ExceptRun(lineTerminators + "\\[/"); c {
+		case -1:
+			t.Err = io.ErrUnexpectedEOF
+			return t.Error()
+		case '\\':
+			if !j.regexpBackslashSequence(t) {
+				return t.Error()
+			}
+		case '[':
+			if !j.regexpExpressionClass(t) {
+				return t.Error()
+			}
+		case '/':
+			t.Except("")
+			break Loop
+		default:
+			t.Except("")
+			if strings.ContainsRune(lineTerminators, c) {
+				t.Err = errors.WithContext("invalid regexp character: ", errors.Error(t.Get()))
+				return t.Error()
+			}
+		}
+	}
+	for {
+		if c := t.Peek(); !isIDContinue(c) {
+			break
+		}
+		t.Except("")
+	}
+	return parser.Token{
+		Type: TokenRegularExpressionLiteral,
+		Data: t.Get(),
+	}, j.inputElement
+}
+
+func (j *jsParser) number(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	if t.Accept("0") {
+		if t.Accept("bB") {
+			if !t.Accept(binaryDigit) {
+				t.Except("")
+				t.Err = errors.WithContext("invalid binary number: ", errors.Error(t.Get()))
+				return t.Error()
+			}
+			t.AcceptRun(binaryDigit)
+		} else if t.Accept("oO") {
+			if !t.Accept(octalDigit) {
+				t.Except("")
+				t.Err = errors.WithContext("invalid octal number: ", errors.Error(t.Get()))
+				return t.Error()
+			}
+			t.AcceptRun(octalDigit)
+		} else if t.Accept("xX") {
+			if !t.Accept(hexDigit) {
+				t.Except("")
+				t.Err = errors.WithContext("invalid hex number: ", errors.Error(t.Get()))
+				return t.Error()
+			}
+			t.AcceptRun(hexDigit)
+		}
+	} else {
+		t.AcceptRun(decimalDigit)
+		if t.Accept(".") {
+			t.AcceptRun(decimalDigit)
+		}
+		if t.Accept("eE") {
+			t.Accept("+-")
+			t.AcceptRun(decimalDigit)
+		}
+	}
+	return parser.Token{
+		Type: TokenNumericLiteral,
+		Data: t.Get(),
+	}, j.inputElement
+}
+
+func (j *jsParser) identifier(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	c := t.Peek()
+	t.Except("")
+	if c == '\\' {
+		if !t.Accept("u") {
+			t.Except("")
+			t.Err = errors.WithContext("unexpected backslash: ", errors.Error(t.Get()))
+			return t.Error()
+		}
+		if !j.unicodeEscapeSequence(t) {
+			t.Except("")
+			return t.Error()
+		}
+	}
+	for {
+		c = t.Peek()
+		if isIDContinue(c) {
+			t.Except("")
+			continue
+		}
+		break
+	}
+	return parser.Token{
+		Type: TokenIdentifier,
+		Data: t.Get(),
+	}, j.inputElement
+}
+
+func (j *jsParser) unicodeEscapeSequence(t *parser.Tokeniser) bool {
+	if t.Accept("{") {
+		if !t.Accept(hexDigit) {
+			t.Except("")
+			t.Err = errors.WithContext("expecting hex digit: ", errors.Error(t.Get()))
+			return false
+		}
+		t.AcceptRun(hexDigit)
+		if !t.Accept("}") {
+			t.Err = errors.WithContext("expecting ending unicode brace: ", errors.Error(t.Get()))
+			return false
+		}
+	} else if !t.Accept(hexDigit) || !t.Accept(hexDigit) || !t.Accept(hexDigit) || !t.Accept(hexDigit) {
+		t.Except("")
+		return false
+	}
+	return true
+}
+
+func (j *jsParser) lastDepth() rune {
+	if len(j.tokenDepth) == 0 {
+		return -1
+	}
+	return rune(j.tokenDepth[len(j.tokenDepth)-1])
+}
+
+func (j *jsParser) escapeSequence(t *parser.Tokeniser) bool {
+	t.Accept("\\")
+	if t.Accept("x") {
+		return t.Accept(hexDigit) && t.Accept(hexDigit)
+	} else if t.Accept("u") {
+		return j.unicodeEscapeSequence(t)
+	} else if t.Accept("0") {
+		return !t.Accept(decimalDigit)
+	}
+	return t.Accept(singleEscapeChar)
+}
+
+func (j *jsParser) stringToken(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+	const (
+		singleStringChars = "'\\" + lineTerminators
+		doubleStringChars = "\"\\" + lineTerminators
+	)
+	var chars string
+	if t.Peek() == '"' {
+		chars = doubleStringChars
+	} else {
+		chars = singleStringChars
+	}
+	t.Except("")
+Loop:
+	for {
+		switch c := t.ExceptRun(chars); c {
+		case '"', '\'':
+			t.Except("")
+			break Loop
+		case '\\':
+			if j.escapeSequence(t) {
+				continue
+			}
+			if t.Err == nil {
+				t.Err = errors.WithContext("invalid escape sequence: ", errors.Error(t.Get()))
+			}
+		default:
+			t.Err = io.ErrUnexpectedEOF
+			if strings.ContainsRune(lineTerminators, c) {
+				t.Except("")
+				t.Err = errors.WithContext("line terminator in string: ", errors.Error(t.Get()))
+			}
+		}
+		return t.Error()
+	}
+	return parser.Token{
+		Type: TokenStringLiteral,
+		Data: t.Get(),
+	}, j.inputElement
+}
+
+func (j *jsParser) template(t *parser.Tokeniser) (parser.Token, parser.TokenFunc) {
+Loop:
+	for {
+		switch t.ExceptRun("`\\$") {
+		case '`':
+			t.Except("")
+			break Loop
+		case '\\':
+			if j.escapeSequence(t) {
+				continue
+			}
+			if t.Err == nil {
+				t.Err = errors.WithContext("invalid escape sequence: ", errors.Error(t.Get()))
+			}
+			return t.Error()
+		case '$':
+			t.Except("")
+			if t.Accept("{") {
+				j.tokenDepth = append(j.tokenDepth, '$')
+				v := t.Get()
+				var typ parser.TokenType
+				if v[0] == '`' {
+					typ = TokenTemplateHead
+				} else {
+					typ = TokenTemplateMiddle
+				}
+				return parser.Token{
+					Type: typ,
+					Data: v,
+				}, j.inputElement
+			}
+		default:
+			t.Err = io.ErrUnexpectedEOF
+			return t.Error()
+		}
+	}
+	j.divisionAllowed = true
+	v := t.Get()
+	var typ parser.TokenType
+	if v[0] == '`' {
+		typ = TokenNoSubstitutionTemplate
+	} else {
+		typ = TokenTemplateTail
+	}
+	return parser.Token{
+		Type: typ,
+		Data: v,
+	}, j.inputElement
+}
---
+++ tokeniser_test.go
@@ -0,0 +1,529 @@
+package javascript
+
+import (
+	"testing"
+
+	"vimagination.zapto.org/parser"
+)
+
+func TestTokeniser(t *testing.T) {
+	for n, test := range [...]struct {
+		Input  string
+		Output []parser.Token
+	}{
+		{
+			"",
+			[]parser.Token{
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			" ",
+			[]parser.Token{
+				{TokenWhitespace, " "},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			" 	",
+			[]parser.Token{
+				{TokenWhitespace, " 	"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			" \n ",
+			[]parser.Token{
+				{TokenWhitespace, " "},
+				{TokenLineTerminator, "\n"},
+				{TokenWhitespace, " "},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\\\"\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\\"\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\n\"",
+			[]parser.Token{
+				{parser.TokenError, "line terminator in string: \"\n"},
+			},
+		},
+		{
+			"\"\\n\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\n\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\\0\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\0\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\\x20\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\x20\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\\u2020\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\u2020\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"\\u\"",
+			[]parser.Token{
+				{parser.TokenError, "invalid escape sequence: \"\\u\""},
+			},
+		},
+		{
+			"\"\\up\"",
+			[]parser.Token{
+				{parser.TokenError, "invalid escape sequence: \"\\up"},
+			},
+		},
+		{
+			"\"\\u{20}\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"\\u{20}\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"use strict\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"use strict\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"use\\u{20}strict\\x65!\\0\"",
+			[]parser.Token{
+				{TokenStringLiteral, "\"use\\u{20}strict\\x65!\\0\""},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"\"use strict\";",
+			[]parser.Token{
+				{TokenStringLiteral, "\"use strict\""},
+				{TokenPunctuator, ";"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0",
+			[]parser.Token{
+				{TokenNumericLiteral, "0"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0b0",
+			[]parser.Token{
+				{TokenNumericLiteral, "0b0"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0b1",
+			[]parser.Token{
+				{TokenNumericLiteral, "0b1"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0b1001010101",
+			[]parser.Token{
+				{TokenNumericLiteral, "0b1001010101"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0",
+			[]parser.Token{
+				{TokenNumericLiteral, "0"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"1",
+			[]parser.Token{
+				{TokenNumericLiteral, "1"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"9",
+			[]parser.Token{
+				{TokenNumericLiteral, "9"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"12345678901",
+			[]parser.Token{
+				{TokenNumericLiteral, "12345678901"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"12345678.901",
+			[]parser.Token{
+				{TokenNumericLiteral, "12345678.901"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"12345678901E123",
+			[]parser.Token{
+				{TokenNumericLiteral, "12345678901E123"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"12345678901e+123",
+			[]parser.Token{
+				{TokenNumericLiteral, "12345678901e+123"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"12345678.901E-123",
+			[]parser.Token{
+				{TokenNumericLiteral, "12345678.901E-123"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0x0",
+			[]parser.Token{
+				{TokenNumericLiteral, "0x0"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0xa",
+			[]parser.Token{
+				{TokenNumericLiteral, "0xa"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0xf",
+			[]parser.Token{
+				{TokenNumericLiteral, "0xf"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0x0f",
+			[]parser.Token{
+				{TokenNumericLiteral, "0x0f"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0xaf",
+			[]parser.Token{
+				{TokenNumericLiteral, "0xaf"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"0xDeAdBeEf",
+			[]parser.Token{
+				{TokenNumericLiteral, "0xDeAdBeEf"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"Infinity",
+			[]parser.Token{
+				{TokenNumericLiteral, "Infinity"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"true",
+			[]parser.Token{
+				{TokenBooleanLiteral, "true"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"false",
+			[]parser.Token{
+				{TokenBooleanLiteral, "false"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"hello",
+			[]parser.Token{
+				{TokenIdentifier, "hello"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"this",
+			[]parser.Token{
+				{TokenKeyword, "this"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"function",
+			[]parser.Token{
+				{TokenKeyword, "function"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"/[a-z]+/g",
+			[]parser.Token{
+				{TokenRegularExpressionLiteral, "/[a-z]+/g"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"/[\\n]/g",
+			[]parser.Token{
+				{TokenRegularExpressionLiteral, "/[\\n]/g"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"var a =	 /^ab[cd]*$/ig;",
+			[]parser.Token{
+				{TokenKeyword, "var"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "a"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "="},
+				{TokenWhitespace, "	 "},
+				{TokenRegularExpressionLiteral, "/^ab[cd]*$/ig"},
+				{TokenPunctuator, ";"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"num /= 4",
+			[]parser.Token{
+				{TokenIdentifier, "num"},
+				{TokenWhitespace, " "},
+				{TokenDivPunctuator, "/="},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "4"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"const num = 8 / 4;",
+			[]parser.Token{
+				{TokenKeyword, "const"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "num"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "="},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "8"},
+				{TokenWhitespace, " "},
+				{TokenDivPunctuator, "/"},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "4"},
+				{TokenPunctuator, ";"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"``",
+			[]parser.Token{
+				{TokenNoSubstitutionTemplate, "``"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"`abc`",
+			[]parser.Token{
+				{TokenNoSubstitutionTemplate, "`abc`"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"`ab${ (val.a / 2) + 1 }c${str}`",
+			[]parser.Token{
+				{TokenTemplateHead, "`ab${"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "("},
+				{TokenIdentifier, "val"},
+				{TokenPunctuator, "."},
+				{TokenIdentifier, "a"},
+				{TokenWhitespace, " "},
+				{TokenDivPunctuator, "/"},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "2"},
+				{TokenPunctuator, ")"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "+"},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "1"},
+				{TokenWhitespace, " "},
+				{TokenTemplateMiddle, "}c${"},
+				{TokenIdentifier, "str"},
+				{TokenTemplateTail, "}`"},
+				{parser.TokenDone, ""},
+			},
+		},
+		{
+			"const myFunc = function(aye, bee, cea) {\n	const num = [123, 4, lastNum(aye, \"beep\", () => window, val => val * 2, (myVar) => {myVar /= 2;return myVar;})], elm = document.getElementByID();\n	console.log(bee, num, elm);}",
+			[]parser.Token{
+				{TokenKeyword, "const"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "myFunc"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "="},
+				{TokenWhitespace, " "},
+				{TokenKeyword, "function"},
+				{TokenPunctuator, "("},
+				{TokenIdentifier, "aye"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "bee"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "cea"},
+				{TokenPunctuator, ")"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "{"},
+				{TokenLineTerminator, "\n"},
+				{TokenWhitespace, "	"},
+				{TokenKeyword, "const"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "num"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "="},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "["},
+				{TokenNumericLiteral, "123"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "4"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "lastNum"},
+				{TokenPunctuator, "("},
+				{TokenIdentifier, "aye"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenStringLiteral, "\"beep\""},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "("},
+				{TokenPunctuator, ")"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "=>"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "window"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "val"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "=>"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "val"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "*"},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "2"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "("},
+				{TokenIdentifier, "myVar"},
+				{TokenPunctuator, ")"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "=>"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "{"},
+				{TokenIdentifier, "myVar"},
+				{TokenWhitespace, " "},
+				{TokenDivPunctuator, "/="},
+				{TokenWhitespace, " "},
+				{TokenNumericLiteral, "2"},
+				{TokenPunctuator, ";"},
+				{TokenKeyword, "return"},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "myVar"},
+				{TokenPunctuator, ";"},
+				{TokenRightBracePunctuator, "}"},
+				{TokenPunctuator, ")"},
+				{TokenPunctuator, "]"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "elm"},
+				{TokenWhitespace, " "},
+				{TokenPunctuator, "="},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "document"},
+				{TokenPunctuator, "."},
+				{TokenIdentifier, "getElementByID"},
+				{TokenPunctuator, "("},
+				{TokenPunctuator, ")"},
+				{TokenPunctuator, ";"},
+				{TokenLineTerminator, "\n"},
+				{TokenWhitespace, "	"},
+				{TokenIdentifier, "console"},
+				{TokenPunctuator, "."},
+				{TokenIdentifier, "log"},
+				{TokenPunctuator, "("},
+				{TokenIdentifier, "bee"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "num"},
+				{TokenPunctuator, ","},
+				{TokenWhitespace, " "},
+				{TokenIdentifier, "elm"},
+				{TokenPunctuator, ")"},
+				{TokenPunctuator, ";"},
+				{TokenRightBracePunctuator, "}"},
+				{parser.TokenDone, ""},
+			},
+		},
+	} {
+		p := parser.NewStringTokeniser(test.Input)
+		var j jsParser
+		p.TokeniserState(j.inputElement)
+		for m, tkn := range test.Output {
+			tk, _ := p.GetToken()
+			if tk.Type != tkn.Type {
+				if tk.Type == parser.TokenError {
+					t.Errorf("test %d.%d: unexpected error: %s", n+1, m+1, tk.Data)
+				} else {
+					t.Errorf("test %d.%d: Incorrect type, expecting %d, got %d", n+1, m+1, tkn.Type, tk.Type)
+				}
+				break
+			} else if tk.Data != tkn.Data {
+				t.Errorf("test %d.%d: Incorrect data, expecting %q, got %q", n+1, m+1, tkn.Data, tk.Data)
+				break
+			}
+		}
+	}
+}
