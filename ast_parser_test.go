4fb944d37fecca677771e4aec3c2b58e2c6d33ef
Michael Woolnough <michael.woolnough@gmail.com>
2019-04-09 10:28:18 +0100

    added simple jsParser next tester

--- ast_parser_test.go
+++ ast_parser_test.go
@@ -78,3 +78,66 @@ func TestNewJSParser(t *testing.T) {
 		}
 	}
 }
+
+func TestJSParserNext(t *testing.T) {
+	j := jsParser{
+		{
+			parser.Token{TokenStringLiteral, "\"use strict\""},
+			0, 0, 0,
+		},
+		{
+			parser.Token{TokenPunctuator, ";"},
+			12, 0, 12,
+		},
+		{
+			parser.Token{TokenLineTerminator, "\n\n"},
+			13, 0, 13,
+		},
+		{
+			parser.Token{TokenKeyword, "var"},
+			15, 2, 0,
+		},
+		{
+			parser.Token{TokenWhitespace, " "},
+			18, 2, 3,
+		},
+		{
+			parser.Token{TokenIdentifier, "hello"},
+			19, 2, 4,
+		},
+		{
+			parser.Token{TokenWhitespace, " "},
+			24, 2, 9,
+		},
+		{
+			parser.Token{TokenPunctuator, "="},
+			25, 2, 10,
+		},
+		{
+			parser.Token{TokenWhitespace, " "},
+			26, 2, 11,
+		},
+		{
+			parser.Token{TokenNoSubstitutionTemplate, "`World\n!`"},
+			27, 2, 12,
+		},
+		{
+			parser.Token{TokenPunctuator, ";"},
+			36, 3, 2,
+		},
+		{
+			parser.Token{parser.TokenDone, ""},
+			37, 3, 3,
+		},
+	}
+	j = j[:0:len(j)]
+	for n, tk := range j[:cap(j)] {
+		tkn := j.next()
+		if tkn != tk {
+			t.Errorf("test %d: expecting %v, got %v", n+1, tk, tkn)
+		}
+	}
+	if tk := j.next(); tk.Type != parser.TokenDone {
+		t.Errorf("test %d: expecting TokenDone, got %v", cap(j)+1, tk)
+	}
+}
