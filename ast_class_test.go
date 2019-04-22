3743a91d5ef16e0ebcc5fedeb6a86fb1d0132a6d
Michael Woolnough <michael.woolnough@gmail.com>
2019-04-22 19:24:27 +0100

    added more Class tests

--- ast_class_test.go
+++ ast_class_test.go
@@ -61,6 +61,91 @@ func TestClassDeclaration(t *testing.T) {
 				Tokens: tk[:11],
 			}
 		}},
+		{`class myClass {method(arg1, arg2){}}`, func(t *test, tk Tokens) {
+			t.Output = ClassDeclaration{
+				BindingIdentifier: &BindingIdentifier{Identifier: &tk[2]},
+				ClassBody: ClassBody{
+					Methods: []MethodDefinition{
+						{
+							PropertyName: PropertyName{
+								LiteralPropertyName: &tk[5],
+								Tokens:              tk[5:6],
+							},
+							Params: FormalParameters{
+								FormalParameterList: []BindingElement{
+									{
+										SingleNameBinding: &BindingIdentifier{Identifier: &tk[7]},
+										Tokens:            tk[7:8],
+									},
+									{
+										SingleNameBinding: &BindingIdentifier{Identifier: &tk[10]},
+										Tokens:            tk[10:11],
+									},
+								},
+								Tokens: tk[7:11],
+							},
+							FunctionBody: Block{
+								Tokens: tk[12:14],
+							},
+							Tokens: tk[5:14],
+						},
+					},
+					Tokens: tk[5:14],
+				},
+				Tokens: tk[:15],
+			}
+		}},
+		{`class myClass {set method(...args){}}`, func(t *test, tk Tokens) {
+			t.Output = ClassDeclaration{
+				BindingIdentifier: &BindingIdentifier{Identifier: &tk[2]},
+				ClassBody: ClassBody{
+					Methods: []MethodDefinition{
+						{
+							Type: MethodSetter,
+							PropertyName: PropertyName{
+								LiteralPropertyName: &tk[7],
+								Tokens:              tk[7:8],
+							},
+							Params: FormalParameters{
+								FunctionRestParameter: &FunctionRestParameter{
+									BindingIdentifier: &BindingIdentifier{Identifier: &tk[10]},
+									Tokens:            tk[10:11],
+								},
+								Tokens: tk[9:11],
+							},
+							FunctionBody: Block{
+								Tokens: tk[12:14],
+							},
+							Tokens: tk[5:14],
+						},
+					},
+					Tokens: tk[5:14],
+				},
+				Tokens: tk[:15],
+			}
+		}},
+		{`class myClass {get value(){}}`, func(t *test, tk Tokens) {
+			t.Output = ClassDeclaration{
+				BindingIdentifier: &BindingIdentifier{Identifier: &tk[2]},
+				ClassBody: ClassBody{
+					Methods: []MethodDefinition{
+						{
+							Type: MethodGetter,
+							PropertyName: PropertyName{
+								LiteralPropertyName: &tk[7],
+								Tokens:              tk[7:8],
+							},
+							FunctionBody: Block{
+								Tokens: tk[10:12],
+							},
+							Tokens: tk[5:12],
+						},
+					},
+					Tokens: tk[5:12],
+				},
+				Tokens: tk[:13],
+			}
+		}},
 	}, func(t *test) (interface{}, error) {
 		return t.Tokens.parseClassDeclaration(t.Yield, t.Await, t.Def)
 	})
