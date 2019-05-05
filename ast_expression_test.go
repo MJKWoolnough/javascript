9b7e1e1cb23d2887ec89bfc5e4fcdcf07bf849de
Michael Woolnough <michael.woolnough@gmail.com>
2019-05-05 17:05:31 +0100

    added more expression tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -16,6 +16,106 @@ func TestNewExpression(t *testing.T) {
 				Tokens: tk[:1],
 			}
 		}},
+		{`someIdentifier`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						IdentifierReference: &IdentifierReference{Identifier: &tk[0]},
+						Tokens:              tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`null`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						Literal: &tk[0],
+						Tokens:  tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`true`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						Literal: &tk[0],
+						Tokens:  tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`false`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						Literal: &tk[0],
+						Tokens:  tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`0`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						Literal: &tk[0],
+						Tokens:  tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`"Hello"`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						Literal: &tk[0],
+						Tokens:  tk[:1],
+					},
+					Tokens: tk[:1],
+				},
+				Tokens: tk[:1],
+			}
+		}},
+		{`[]`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						ArrayLiteral: &ArrayLiteral{
+							Tokens: tk[:2],
+						},
+						Tokens: tk[:2],
+					},
+					Tokens: tk[:2],
+				},
+				Tokens: tk[:2],
+			}
+		}},
+		{`{}`, func(t *test, tk Tokens) {
+			t.Output = NewExpression{
+				MemberExpression: MemberExpression{
+					PrimaryExpression: &PrimaryExpression{
+						ObjectLiteral: &ObjectLiteral{
+							Tokens: tk[:2],
+						},
+						Tokens: tk[:2],
+					},
+					Tokens: tk[:2],
+				},
+				Tokens: tk[:2],
+			}
+		}},
 	}, func(t *test) (interface{}, error) {
 		return t.Tokens.parseNewExpression(t.Yield, t.Await)
 	})
