ed71f4bda91e5d46afb3a5b263de3cb4759d0fa6
Michael Woolnough <michael.woolnough@gmail.com>
2019-05-21 08:39:07 +0100

    simplified writing conditional tests

--- ast_conditional_test.go
+++ ast_conditional_test.go
@@ -60,6 +60,76 @@ func makeConditionLiteral(tk Tokens, pos int) ConditionalExpression {
 	}
 }
 
+func wrapConditional(p interface{}) ConditionalExpression {
+	var c ConditionalExpression
+	switch p := p.(type) {
+	case LogicalORExpression:
+		c.LogicalORExpression = p
+		goto logicalORExpression
+	case LogicalANDExpression:
+		c.LogicalORExpression.LogicalANDExpression = p
+		goto logicalANDExpression
+	case BitwiseORExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression = p
+		goto bitwiseORExpression
+	case BitwiseXORExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression = p
+		goto bitwiseXORExpression
+	case BitwiseANDExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression = p
+		goto bitwiseANDExpression
+	case EqualityExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression = p
+		goto equalityExpression
+	case RelationalExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression = p
+		goto relationalExpression
+	case ShiftExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression = p
+		goto shiftExpression
+	case AdditiveExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression = p
+		goto additiveExpression
+	case MultiplicativeExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression = p
+		goto multiplicativeExpression
+	case ExponentiationExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression = p
+		goto exponentiationExpression
+	case UnaryExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression.UnaryExpression = p
+		goto unaryExpression
+	case UpdateExpression:
+		c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression.UnaryExpression.UpdateExpression = p
+	}
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.MultiplicativeExpression.ExponentiationExpression.UnaryExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression.UnaryExpression.UpdateExpression.Tokens
+unaryExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.MultiplicativeExpression.ExponentiationExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression.UnaryExpression.Tokens
+exponentiationExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.ExponentiationExpression.Tokens
+multiplicativeExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.MultiplicativeExpression.Tokens
+additiveExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.AdditiveExpression.Tokens
+shiftExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.ShiftExpression.Tokens
+relationalExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.RelationalExpression.Tokens
+equalityExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.EqualityExpression.Tokens
+bitwiseANDExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.BitwiseANDExpression.Tokens
+bitwiseXORExpression:
+	c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression.Tokens
+bitwiseORExpression:
+	c.LogicalORExpression.LogicalANDExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.Tokens
+logicalANDExpression:
+	c.LogicalORExpression.Tokens = c.LogicalORExpression.LogicalANDExpression.Tokens
+logicalORExpression:
+	c.Tokens = c.LogicalORExpression.Tokens
+	return c
+}
+
 func TestConditional(t *testing.T) {
 	doTests(t, []sourceFn{
 		{`true`, func(t *test, tk Tokens) {
@@ -68,20 +138,58 @@ func TestConditional(t *testing.T) {
 		{`true || false`, func(t *test, tk Tokens) {
 			litA := makeConditionLiteral(tk, 0)
 			litB := makeConditionLiteral(tk, 4)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalORExpression:  &litA.LogicalORExpression,
-					LogicalANDExpression: litB.LogicalORExpression.LogicalANDExpression,
-					Tokens:               tk[:5],
-				},
-				Tokens: tk[:5],
-			}
+			t.Output = wrapConditional(LogicalORExpression{
+				LogicalORExpression:  &litA.LogicalORExpression,
+				LogicalANDExpression: litB.LogicalORExpression.LogicalANDExpression,
+				Tokens:               tk[:5],
+			})
 		}},
 		{`true && false`, func(t *test, tk Tokens) {
 			litA := makeConditionLiteral(tk, 0)
 			litB := makeConditionLiteral(tk, 4)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
+			t.Output = wrapConditional(LogicalORExpression{
+				LogicalANDExpression: LogicalANDExpression{
+					LogicalANDExpression: &litA.LogicalORExpression.LogicalANDExpression,
+					BitwiseORExpression:  litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
+					Tokens:               tk[:5],
+				},
+				Tokens: tk[:5],
+			})
+		}},
+		{`1 || 2 || 3`, func(t *test, tk Tokens) {
+			litA := makeConditionLiteral(tk, 0)
+			litB := makeConditionLiteral(tk, 4)
+			litC := makeConditionLiteral(tk, 8)
+			t.Output = wrapConditional(LogicalORExpression{
+				LogicalORExpression: &LogicalORExpression{
+					LogicalORExpression:  &litA.LogicalORExpression,
+					LogicalANDExpression: litB.LogicalORExpression.LogicalANDExpression,
+					Tokens:               tk[:5],
+				},
+				LogicalANDExpression: litC.LogicalORExpression.LogicalANDExpression,
+				Tokens:               tk[:9],
+			})
+		}},
+		{`1 && 2 && 3`, func(t *test, tk Tokens) {
+			litA := makeConditionLiteral(tk, 0)
+			litB := makeConditionLiteral(tk, 4)
+			litC := makeConditionLiteral(tk, 8)
+			t.Output = wrapConditional(LogicalANDExpression{
+				LogicalANDExpression: &LogicalANDExpression{
+					LogicalANDExpression: &litA.LogicalORExpression.LogicalANDExpression,
+					BitwiseORExpression:  litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
+					Tokens:               tk[:5],
+				},
+				BitwiseORExpression: litC.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
+				Tokens:              tk[:9],
+			})
+		}},
+		{`1 && 2 || 3`, func(t *test, tk Tokens) {
+			litA := makeConditionLiteral(tk, 0)
+			litB := makeConditionLiteral(tk, 4)
+			litC := makeConditionLiteral(tk, 8)
+			t.Output = wrapConditional(LogicalORExpression{
+				LogicalORExpression: &LogicalORExpression{
 					LogicalANDExpression: LogicalANDExpression{
 						LogicalANDExpression: &litA.LogicalORExpression.LogicalANDExpression,
 						BitwiseORExpression:  litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
@@ -89,100 +197,32 @@ func TestConditional(t *testing.T) {
 					},
 					Tokens: tk[:5],
 				},
-				Tokens: tk[:5],
-			}
-		}},
-		{`1 || 2 || 3`, func(t *test, tk Tokens) {
-			litA := makeConditionLiteral(tk, 0)
-			litB := makeConditionLiteral(tk, 4)
-			litC := makeConditionLiteral(tk, 8)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalORExpression: &LogicalORExpression{
-						LogicalORExpression:  &litA.LogicalORExpression,
-						LogicalANDExpression: litB.LogicalORExpression.LogicalANDExpression,
-						Tokens:               tk[:5],
-					},
-					LogicalANDExpression: litC.LogicalORExpression.LogicalANDExpression,
-					Tokens:               tk[:9],
-				},
-				Tokens: tk[:9],
-			}
-		}},
-		{`1 && 2 && 3`, func(t *test, tk Tokens) {
-			litA := makeConditionLiteral(tk, 0)
-			litB := makeConditionLiteral(tk, 4)
-			litC := makeConditionLiteral(tk, 8)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalANDExpression: LogicalANDExpression{
-						LogicalANDExpression: &LogicalANDExpression{
-							LogicalANDExpression: &litA.LogicalORExpression.LogicalANDExpression,
-							BitwiseORExpression:  litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
-							Tokens:               tk[:5],
-						},
-						BitwiseORExpression: litC.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
-						Tokens:              tk[:9],
-					},
-					Tokens: tk[:9],
-				},
-				Tokens: tk[:9],
-			}
-		}},
-		{`1 && 2 || 3`, func(t *test, tk Tokens) {
-			litA := makeConditionLiteral(tk, 0)
-			litB := makeConditionLiteral(tk, 4)
-			litC := makeConditionLiteral(tk, 8)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalORExpression: &LogicalORExpression{
-						LogicalANDExpression: LogicalANDExpression{
-							LogicalANDExpression: &litA.LogicalORExpression.LogicalANDExpression,
-							BitwiseORExpression:  litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
-							Tokens:               tk[:5],
-						},
-						Tokens: tk[:5],
-					},
-					LogicalANDExpression: litC.LogicalORExpression.LogicalANDExpression,
-					Tokens:               tk[:9],
-				},
-				Tokens: tk[:9],
-			}
+				LogicalANDExpression: litC.LogicalORExpression.LogicalANDExpression,
+				Tokens:               tk[:9],
+			})
 		}},
 		{`1 || 2 && 3`, func(t *test, tk Tokens) {
 			litA := makeConditionLiteral(tk, 0)
 			litB := makeConditionLiteral(tk, 4)
 			litC := makeConditionLiteral(tk, 8)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalORExpression: &litA.LogicalORExpression,
-					LogicalANDExpression: LogicalANDExpression{
-						LogicalANDExpression: &litB.LogicalORExpression.LogicalANDExpression,
-						BitwiseORExpression:  litC.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
-						Tokens:               tk[4:9],
-					},
-					Tokens: tk[:9],
+			t.Output = wrapConditional(LogicalORExpression{
+				LogicalORExpression: &litA.LogicalORExpression,
+				LogicalANDExpression: LogicalANDExpression{
+					LogicalANDExpression: &litB.LogicalORExpression.LogicalANDExpression,
+					BitwiseORExpression:  litC.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
+					Tokens:               tk[4:9],
 				},
 				Tokens: tk[:9],
-			}
+			})
 		}},
 		{`1 | 2`, func(t *test, tk Tokens) {
 			litA := makeConditionLiteral(tk, 0)
 			litB := makeConditionLiteral(tk, 4)
-			t.Output = ConditionalExpression{
-				LogicalORExpression: LogicalORExpression{
-					LogicalANDExpression: LogicalANDExpression{
-						BitwiseORExpression: BitwiseORExpression{
-							BitwiseORExpression:  &litA.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
-							BitwiseXORExpression: litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression,
-							Tokens:               tk[:5],
-						},
-						Tokens: tk[:5],
-					},
-					Tokens: tk[:5],
-				},
-				Tokens: tk[:5],
-			}
+			t.Output = wrapConditional(BitwiseORExpression{
+				BitwiseORExpression:  &litA.LogicalORExpression.LogicalANDExpression.BitwiseORExpression,
+				BitwiseXORExpression: litB.LogicalORExpression.LogicalANDExpression.BitwiseORExpression.BitwiseXORExpression,
+				Tokens:               tk[:5],
+			})
 		}},
 	}, func(t *test) (interface{}, error) {
 		return t.Tokens.parseConditionalExpression(t.In, t.Yield, t.Await)
