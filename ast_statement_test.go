d4fc4a61157e11951868e92b94dab750ad817283
Michael Woolnough <michael.woolnough@gmail.com>
2019-05-23 14:58:21 +0100

    Added 'IF/ELSE' test

--- ast_statement_test.go
+++ ast_statement_test.go
@@ -81,6 +81,36 @@ func TestStatement(t *testing.T) {
 				Tokens: tk[:8],
 			}
 		}},
+		{`if (1) {} else {}`, func(t *test, tk Tokens) {
+			litA := makeConditionLiteral(tk, 3)
+			t.Output = Statement{
+				IfStatement: &IfStatement{
+					Expression: Expression{
+						Expressions: []AssignmentExpression{
+							{
+								ConditionalExpression: &litA,
+								Tokens:                tk[3:4],
+							},
+						},
+						Tokens: tk[3:4],
+					},
+					Statement: Statement{
+						BlockStatement: &Block{
+							Tokens: tk[6:8],
+						},
+						Tokens: tk[6:8],
+					},
+					ElseStatement: &Statement{
+						BlockStatement: &Block{
+							Tokens: tk[11:13],
+						},
+						Tokens: tk[11:13],
+					},
+					Tokens: tk[:13],
+				},
+				Tokens: tk[:13],
+			}
+		}},
 	}, func(t *test) (interface{}, error) {
 		return t.Tokens.parseStatement(t.Yield, t.Await, t.Ret)
 	})
