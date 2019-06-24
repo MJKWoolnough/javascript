bb200f6f8e137139033c2dd96f13b1e32f6564b8
Michael Woolnough <michael.woolnough@gmail.com>
2019-06-24 10:55:00 +0100

    added first ExportDeclaration tests

--- ast_module_test.go
+++ ast_module_test.go
@@ -228,6 +228,233 @@ func TestModule(t *testing.T) {
 				Tokens: tk[:13],
 			}
 		}},
+		{`export {};`, func(t *test, tk Tokens) { // 9
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								Tokens: tk[2:4],
+							},
+							Tokens: tk[:5],
+						},
+						Tokens: tk[:5],
+					},
+				},
+				Tokens: tk[:5],
+			}
+		}},
+		{`export {a};`, func(t *test, tk Tokens) { // 10
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName: &tk[3],
+										Tokens:         tk[3:4],
+									},
+								},
+								Tokens: tk[2:5],
+							},
+							Tokens: tk[:6],
+						},
+						Tokens: tk[:6],
+					},
+				},
+				Tokens: tk[:6],
+			}
+		}},
+		{`export {a as b};`, func(t *test, tk Tokens) { // 11
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName:  &tk[3],
+										EIdentifierName: &tk[7],
+										Tokens:          tk[3:8],
+									},
+								},
+								Tokens: tk[2:9],
+							},
+							Tokens: tk[:10],
+						},
+						Tokens: tk[:10],
+					},
+				},
+				Tokens: tk[:10],
+			}
+		}},
+		{`export {a as b, c as d, e, f};`, func(t *test, tk Tokens) { // 12
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName:  &tk[3],
+										EIdentifierName: &tk[7],
+										Tokens:          tk[3:8],
+									},
+									{
+										IdentifierName:  &tk[10],
+										EIdentifierName: &tk[14],
+										Tokens:          tk[10:15],
+									},
+									{
+										IdentifierName: &tk[17],
+										Tokens:         tk[17:18],
+									},
+									{
+										IdentifierName: &tk[20],
+										Tokens:         tk[20:21],
+									},
+								},
+								Tokens: tk[2:22],
+							},
+							Tokens: tk[:23],
+						},
+						Tokens: tk[:23],
+					},
+				},
+				Tokens: tk[:23],
+			}
+		}},
+		{`export * from 'a';`, func(t *test, tk Tokens) { // 13
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							FromClause: &FromClause{
+								ModuleSpecifier: &tk[6],
+								Tokens:          tk[4:7],
+							},
+							Tokens: tk[:8],
+						},
+						Tokens: tk[:8],
+					},
+				},
+				Tokens: tk[:8],
+			}
+		}},
+		{`export {} from 'a';`, func(t *test, tk Tokens) { // 14
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								Tokens: tk[2:4],
+							},
+							FromClause: &FromClause{
+								ModuleSpecifier: &tk[7],
+								Tokens:          tk[5:8],
+							},
+							Tokens: tk[:9],
+						},
+						Tokens: tk[:9],
+					},
+				},
+				Tokens: tk[:9],
+			}
+		}},
+		{`export {a} from 'b';`, func(t *test, tk Tokens) { // 15
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName: &tk[3],
+										Tokens:         tk[3:4],
+									},
+								},
+								Tokens: tk[2:5],
+							},
+							FromClause: &FromClause{
+								ModuleSpecifier: &tk[8],
+								Tokens:          tk[6:9],
+							},
+							Tokens: tk[:10],
+						},
+						Tokens: tk[:10],
+					},
+				},
+				Tokens: tk[:10],
+			}
+		}},
+		{`export {a as b} from 'c';`, func(t *test, tk Tokens) { // 16
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName:  &tk[3],
+										EIdentifierName: &tk[7],
+										Tokens:          tk[3:8],
+									},
+								},
+								Tokens: tk[2:9],
+							},
+							FromClause: &FromClause{
+								ModuleSpecifier: &tk[12],
+								Tokens:          tk[10:13],
+							},
+							Tokens: tk[:14],
+						},
+						Tokens: tk[:14],
+					},
+				},
+				Tokens: tk[:14],
+			}
+		}},
+		{`export {a as b, c as d, e, f} from 'g';`, func(t *test, tk Tokens) { // 17
+			t.Output = Module{
+				ModuleListItems: []ModuleListItem{
+					{
+						ExportDeclaration: &ExportDeclaration{
+							ExportClause: &ExportClause{
+								ExportList: []ExportSpecifier{
+									{
+										IdentifierName:  &tk[3],
+										EIdentifierName: &tk[7],
+										Tokens:          tk[3:8],
+									},
+									{
+										IdentifierName:  &tk[10],
+										EIdentifierName: &tk[14],
+										Tokens:          tk[10:15],
+									},
+									{
+										IdentifierName: &tk[17],
+										Tokens:         tk[17:18],
+									},
+									{
+										IdentifierName: &tk[20],
+										Tokens:         tk[20:21],
+									},
+								},
+								Tokens: tk[2:22],
+							},
+							FromClause: &FromClause{
+								ModuleSpecifier: &tk[25],
+								Tokens:          tk[23:26],
+							},
+							Tokens: tk[:27],
+						},
+						Tokens: tk[:27],
+					},
+				},
+				Tokens: tk[:27],
+			}
+		}},
 	}, func(t *test) (interface{}, error) {
 		return t.Tokens.parseModule()
 	})
