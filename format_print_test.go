1a4368e9c1b7ad222563b7d20c84cb8d5c8d8263
Michael Woolnough <michael.woolnough@gmail.com>
2019-07-27 20:33:30 +0100

    Added start of module print testing

--- format_print_test.go
+++ format_print_test.go
@@ -178,3 +178,28 @@ a++
 		}
 	}
 }
+
+func TestModulePrinting(t *testing.T) {
+	var st state
+	for n, test := range [...]struct {
+		Input, Output string
+		Verbose       bool
+	}{
+		{ // 1
+			Input:  `1;`,
+			Output: `1;`,
+		},
+	} {
+		m, err := ParseModule(parser.NewStringTokeniser(test.Input))
+		if err != nil {
+			t.Errorf("test %d: unexpected error: %s", n+1, err)
+			continue
+		}
+		st.Buffer = st.Buffer[:0]
+		st.Verbose = test.Verbose
+		m.Format(&st, 's')
+		if str := string(st.Buffer); str != test.Output {
+			t.Errorf("test %d: expecting %q, got %q\n%s", n+1, test.Output, str, m)
+		}
+	}
+}
