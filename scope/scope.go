3f1b5ce2ed8b89bc151e162a6105ec3ce6c68807
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-10 09:18:48 +0100

    wrote processArguments fn

--- scope/scope.go
+++ scope/scope.go
@@ -853,6 +853,16 @@ func processMemberExpression(m *javascript.MemberExpression, scope *Scope, set b
 }
 
 func processArguments(a *javascript.Arguments, scope *Scope, set bool) error {
+	for n := range a.ArgumentList {
+		if err := processAssignmentExpression(&a.ArgumentList[n], scope, set); err != nil {
+			return err
+		}
+	}
+	if a.SpreadArgument != nil {
+		if err := processAssignmentExpression(a.SpreadArgument, scope, set); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
