b6ae46ea12c8c7a7dc00af80cd54ca1212627f39
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-14 09:55:36 +0100

    added testing framework

---
+++ scope/scope_test.go
@@ -0,0 +1,36 @@
+package scope
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/parser"
+)
+
+func TestScriptScope(t *testing.T) {
+	for n, test := range [...]struct {
+		Input  string
+		Output func(*javascript.Script) (*Scope, error)
+	}{} {
+		source, err := javascript.ParseScript(parser.NewStringTokeniser(test.Input))
+		if err != nil {
+			t.Errorf("test %d: unexpected error parsing script: %s", n+1, err)
+		} else {
+			tscope, terr := test.Output(source)
+			scope, err := ScriptScope(source, nil)
+			if terr != nil && err != nil {
+				if !errors.Is(terr, err) {
+					t.Errorf("test %d: expecting error: %s\ngot: %s", n+1, terr, err)
+				}
+			} else if terr != nil {
+				t.Errorf("test %d: received no error when expecting: %s", n+1, terr)
+			} else if err != nil {
+				t.Errorf("test %d: receieved error when expecting none: %s", n+1, err)
+			} else if !reflect.DeepEqual(scope, tscope) {
+				t.Errorf("test %d: result did not match expected", n+1)
+			}
+		}
+	}
+}
