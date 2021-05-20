365bce7c5409b2eb7c5444dbe562a14d2d1a3179
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-20 11:28:10 +0100

    test for var scope inside of catch block

--- scope/scope_test.go
+++ scope/scope_test.go
@@ -2021,6 +2021,62 @@ func TestScriptScope(t *testing.T) {
 				return scope, nil
 			},
 		},
+		{ // 54
+			`function a() {try{}catch(b){var a}}`,
+			func(s *javascript.Script) (*Scope, error) {
+				scope := new(Scope)
+				fscope := &Scope{
+					Parent: scope,
+				}
+				tscope := &Scope{
+					IsLexicalScope: true,
+					Parent:         fscope,
+					Scopes:         make(map[fmt.Formatter]*Scope),
+				}
+				tscope.Bindings = map[string][]Binding{
+					"b": []Binding{
+						{
+							BindingType: BindingCatch,
+							Scope:       tscope,
+							Token:       s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[0].Statement.TryStatement.CatchParameterBindingIdentifier,
+						},
+					},
+					"a": []Binding{
+						{
+							BindingType: BindingVar,
+							Scope:       tscope,
+							Token:       s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[0].Statement.TryStatement.CatchBlock.StatementList[0].Statement.VariableStatement.VariableDeclarationList[0].BindingIdentifier,
+						},
+					},
+				}
+
+				fscope.Scopes = map[fmt.Formatter]*Scope{
+					&s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[0].Statement.TryStatement.TryBlock: &Scope{
+						IsLexicalScope: true,
+						Parent:         fscope,
+						Scopes:         make(map[fmt.Formatter]*Scope),
+						Bindings:       make(map[string][]Binding),
+					},
+					s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[0].Statement.TryStatement.CatchBlock: tscope,
+				}
+				fscope.Bindings = map[string][]Binding{
+					"this":      []Binding{},
+					"arguments": []Binding{},
+					"a":         tscope.Bindings["a"],
+				}
+				scope.Scopes = map[fmt.Formatter]*Scope{s.StatementList[0].Declaration.FunctionDeclaration: fscope}
+				scope.Bindings = map[string][]Binding{
+					"a": []Binding{
+						{
+							BindingType: BindingHoistable,
+							Scope:       scope,
+							Token:       s.StatementList[0].Declaration.FunctionDeclaration.BindingIdentifier,
+						},
+					},
+				}
+				return scope, nil
+			},
+		},
 	} {
 		source, err := javascript.ParseScript(parser.NewStringTokeniser(test.Input))
 		if err != nil {
