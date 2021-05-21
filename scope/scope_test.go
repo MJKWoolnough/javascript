dc29f695649bcabdd1005b1230d8d592bac3eb0b
Michael Woolnough <michael.woolnough@gmail.com>
2021-05-21 15:30:28 +0100

    added switch test testing lex scope after default case

--- scope/scope_test.go
+++ scope/scope_test.go
@@ -2390,6 +2390,15 @@ func TestScriptScope(t *testing.T) {
 				}
 			},
 		},
+		{ // 62
+			`function a() {let a;switch(a){default:break;case 1:let a;break;case 2:let a}}`,
+			func(s *javascript.Script) (*Scope, error) {
+				return nil, ErrDuplicateDeclaration{
+					Declaration: s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[1].Statement.SwitchStatement.PostDefaultCaseClauses[0].StatementList[0].Declaration.LexicalDeclaration.BindingList[0].BindingIdentifier,
+					Duplicate:   s.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[1].Statement.SwitchStatement.PostDefaultCaseClauses[1].StatementList[0].Declaration.LexicalDeclaration.BindingList[0].BindingIdentifier,
+				}
+			},
+		},
 	} {
 		source, err := javascript.ParseScript(parser.NewStringTokeniser(test.Input))
 		if err != nil {
