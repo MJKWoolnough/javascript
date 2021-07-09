fa4e10261ec3f6eb4798059b48ed01b69b7379af
Michael Woolnough <michael.woolnough@gmail.com>
2021-07-09 07:48:42 +0100

    updated comment for AssignmentExpression

--- ast_expression.go
+++ ast_expression.go
@@ -64,17 +64,22 @@ func (ao *AssignmentOperator) parse(j *jsParser) error {
 // https://262.ecma-international.org/11.0/#prod-AssignmentExpression
 //
 // It is only valid for one of ConditionalExpression, ArrowFunction,
-// LeftHandSideExpression to be non-nil.
+// LeftHandSideExpression, LeftHandSideArray, and LeftHandSideObject to be
+// non-nil.
 //
-// If LeftHandSideExpression is non-nil, then AssignmentOperator must not be
-// AssignmentNone and AssignmentExpression must be non-nil.
+// If LeftHandSideExpression, LeftHandSideArray, or LeftHandSideObject are
+// non-nil, then AssignmentOperator must not be AssignmentNone and
+// AssignmentExpression must be non-nil.
+//
+// If LeftHandSideArray, or LeftHandSideObject are non-nil, AssignmentOperator
+// must be AssignmentAssign.
 //
 // If Yield is true, AssignmentExpression must be non-nil.
 //
 // If AssignmentOperator is AssignmentNone LeftHandSideExpression must be nil.
 //
-// If LeftHandSideExpression is nil and Yield is false, AssignmentExpression
-// must be nil.
+// If LeftHandSideExpression, LeftHandSideArray, and LeftHandSideObject are nil
+// and Yield is false, AssignmentExpression must be nil.
 type AssignmentExpression struct {
 	ConditionalExpression  *ConditionalExpression
 	ArrowFunction          *ArrowFunction
