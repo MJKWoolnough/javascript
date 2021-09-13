283a0c62090afcd800ab0c47b2013d3ee44cd0d1
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-13 09:17:21 +0100

    added ObjectAssignmentPattern error tests

--- ast_expression_test.go
+++ ast_expression_test.go
@@ -1944,6 +1944,40 @@ func TestAssignmentExpression(t *testing.T) {
 				Tokens: tk[:8],
 			}
 		}},
+		{"{...{a}} = b", func(t *test, tk Tokens) { // 43
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err:     ErrBadRestElement,
+						Parsing: "ObjectAssignmentPattern",
+						Token:   tk[2],
+					},
+					Parsing: "AssignmentPattern",
+					Token:   tk[0],
+				},
+				Parsing: "AssignmentExpression",
+				Token:   tk[0],
+			}
+		}},
+		{"{...a=b} = c", func(t *test, tk Tokens) { // 44
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err:     ErrInvalidDestructuringAssignmentTarget,
+							Parsing: "DestructuringAssignmentTarget",
+							Token:   tk[2],
+						},
+						Parsing: "ObjectAssignmentPattern",
+						Token:   tk[2],
+					},
+					Parsing: "AssignmentPattern",
+					Token:   tk[0],
+				},
+				Parsing: "AssignmentExpression",
+				Token:   tk[0],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		var ae AssignmentExpression
 		err := ae.parse(&t.Tokens, t.In, t.Yield, t.Await)
