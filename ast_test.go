2387ff070248a61c86ff60aa751b8d770cc62df7
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-20 12:38:43 +0100

    added ArrayBindingPattern error test

--- ast_test.go
+++ ast_test.go
@@ -3018,6 +3018,29 @@ func TestArrowFunction(t *testing.T) {
 				Token:   tk[0],
 			}
 		}},
+		{"([a /= b]) => 1", func(t *test, tk Tokens) { // 30
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err:     ErrInvalidAssignment,
+								Parsing: "BindingElement",
+								Token:   tk[2],
+							},
+							Parsing: "ArrayBindingPattern",
+							Token:   tk[1],
+						},
+						Parsing: "BindingElement",
+						Token:   tk[1],
+					},
+					Parsing: "FormalParameters",
+					Token:   tk[0],
+				},
+				Parsing: "ArrowFunction",
+				Token:   tk[0],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		var (
 			pe  PrimaryExpression
