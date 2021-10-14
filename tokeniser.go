299a61445024249691a405ebbb2efc2be3248767
Michael Woolnough <michael.woolnough@gmail.com>
2021-10-14 09:58:11 +0100

    added enum to reserved list

--- tokeniser.go
+++ tokeniser.go
@@ -24,7 +24,7 @@ const (
 	zwj  rune = 8205
 )
 
-var keywords = [...]string{"await", "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"}
+var keywords = [...]string{"await", "break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete", "do", "else", "enum", "export", "extends", "finally", "for", "function", "if", "import", "in", "instanceof", "new", "return", "super", "switch", "this", "throw", "try", "typeof", "var", "void", "while", "with", "yield"}
 
 // Javascript Token values
 const (
