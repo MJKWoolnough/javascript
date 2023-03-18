955e6a73e1b4bed626cdba419133367aff32c170
Michael Woolnough <michael.woolnough@gmail.com>
2023-03-18 17:17:54 +0000

    added SkipParameterProperties tests

--- typescript_test.go
+++ typescript_test.go
@@ -82,6 +82,207 @@ func TestTypescriptModule(t *testing.T) {
 				Token:   tk[8],
 			}
 		}},
+		{`class A {
+private;
+public
+protected;
+a
+private A
+protected B;
+public C
+private static D;
+protected static E
+public static F
+}`, func(t *test, tk Tokens) { // 3
+			t.Typescript = true
+			t.Output = Module{
+				ModuleListItems: []ModuleItem{
+					{
+						StatementListItem: &StatementListItem{
+							Declaration: &Declaration{
+								ClassDeclaration: &ClassDeclaration{
+									BindingIdentifier: &tk[2],
+									ClassBody: []ClassElement{
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[6],
+														Tokens:              tk[6:7],
+													},
+													Tokens: tk[6:7],
+												},
+												Tokens: tk[6:7],
+											},
+											Tokens: tk[6:8],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[9],
+														Tokens:              tk[9:10],
+													},
+													Tokens: tk[9:10],
+												},
+												Tokens: tk[9:10],
+											},
+											Tokens: tk[9:10],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[11],
+														Tokens:              tk[11:12],
+													},
+													Tokens: tk[11:12],
+												},
+												Tokens: tk[11:12],
+											},
+											Tokens: tk[11:13],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[14],
+														Tokens:              tk[14:15],
+													},
+													Tokens: tk[14:15],
+												},
+												Tokens: tk[14:15],
+											},
+											Tokens: tk[14:15],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[18],
+														Tokens:              tk[18:19],
+													},
+													Tokens: tk[18:19],
+												},
+												Tokens: tk[18:19],
+											},
+											Tokens: tk[16:19],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[22],
+														Tokens:              tk[22:23],
+													},
+													Tokens: tk[22:23],
+												},
+												Tokens: tk[22:23],
+											},
+											Tokens: tk[20:24],
+										},
+										{
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[27],
+														Tokens:              tk[27:28],
+													},
+													Tokens: tk[27:28],
+												},
+												Tokens: tk[27:28],
+											},
+											Tokens: tk[25:28],
+										},
+										{
+											Static: true,
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[33],
+														Tokens:              tk[33:34],
+													},
+													Tokens: tk[33:34],
+												},
+												Tokens: tk[33:34],
+											},
+											Tokens: tk[29:35],
+										},
+										{
+											Static: true,
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[40],
+														Tokens:              tk[40:41],
+													},
+													Tokens: tk[40:41],
+												},
+												Tokens: tk[40:41],
+											},
+											Tokens: tk[36:41],
+										},
+										{
+											Static: true,
+											FieldDefinition: &FieldDefinition{
+												ClassElementName: ClassElementName{
+													PropertyName: &PropertyName{
+														LiteralPropertyName: &tk[46],
+														Tokens:              tk[46:47],
+													},
+													Tokens: tk[46:47],
+												},
+												Tokens: tk[46:47],
+											},
+											Tokens: tk[42:47],
+										},
+									},
+									Tokens: tk[:49],
+								},
+								Tokens: tk[:49],
+							},
+							Tokens: tk[:49],
+						},
+						Tokens: tk[:49],
+					},
+				},
+				Tokens: tk[:49],
+			}
+		}},
+		{`class A {
+private;
+public
+protected;
+a
+private A
+protected B;
+public C
+private static D;
+protected static E
+public static F
+}`, func(t *test, tk Tokens) { // 4
+			t.Err = Error{
+				Err: Error{
+					Err: Error{
+						Err: Error{
+							Err: Error{
+								Err:     ErrMissingSemiColon,
+								Parsing: "ClassElement",
+								Token:   tk[18],
+							},
+							Parsing: "ClassDeclaration",
+							Token:   tk[16],
+						},
+						Parsing: "Declaration",
+						Token:   tk[0],
+					},
+					Parsing: "StatementListItem",
+					Token:   tk[0],
+				},
+				Parsing: "ModuleItem",
+				Token:   tk[0],
+			}
+		}},
 	}, func(t *test) (Type, error) {
 		if t.Typescript {
 			t.Tokens[:cap(t.Tokens)][cap(t.Tokens)-1].Data = marker
