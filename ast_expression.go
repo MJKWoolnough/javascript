d6273a6c8d9d65037376fbd72a9b07b206daa69b
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-01 19:22:02 +0100

    can now parse abstract class expressions

--- ast_expression.go
+++ ast_expression.go
@@ -942,6 +942,10 @@ type PrimaryExpression struct {
 }
 
 func (pe *PrimaryExpression) parse(j *jsParser, yield, await bool) error {
+	g := j.NewGoal()
+	if g.SkipAbstract() {
+		g.AcceptRunWhitespace()
+	}
 	if j.AcceptToken(parser.Token{Type: TokenKeyword, Data: "this"}) {
 		pe.This = j.GetLastToken()
 	} else if j.Accept(TokenNullLiteral, TokenBooleanLiteral, TokenNumericLiteral, TokenStringLiteral, TokenRegularExpressionLiteral) {
@@ -967,8 +971,11 @@ func (pe *PrimaryExpression) parse(j *jsParser, yield, await bool) error {
 			return j.Error("PrimaryExpression", err)
 		}
 		j.Score(g)
-	} else if t == (parser.Token{Type: TokenKeyword, Data: "class"}) {
+	} else if t == (parser.Token{Type: TokenKeyword, Data: "class"}) || g.Peek() == (parser.Token{Type: TokenKeyword, Data: "class"}) {
 		g := j.NewGoal()
+		if g.SkipAbstract() {
+			g.AcceptRunWhitespace()
+		}
 		pe.ClassExpression = new(ClassDeclaration)
 		if err := pe.ClassExpression.parse(&g, yield, await, true); err != nil {
 			return j.Error("PrimaryExpression", err)
