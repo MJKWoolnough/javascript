35d49e18993e759589dd8da8475f222e03f796ff
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-02 12:21:22 +0100

    using OnOptionalType in ConditionalExpression to determine if we're actually in a ConditionalExpression, or an optional param in an ArrowFunc

--- ast_conditional.go
+++ ast_conditional.go
@@ -42,25 +42,27 @@ func (ce *ConditionalExpression) parse(j *jsParser, in, yield, await bool) error
 		}
 	}
 	if g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "?"}) {
-		j.Score(g)
-		j.AcceptRunWhitespace()
-		g = j.NewGoal()
-		ce.True = new(AssignmentExpression)
-		if err := ce.True.parse(&g, true, yield, await); err != nil {
-			return j.Error("ConditionalExpression", err)
+		if !g.OnOptionalType() {
+			j.Score(g)
+			j.AcceptRunWhitespace()
+			g = j.NewGoal()
+			ce.True = new(AssignmentExpression)
+			if err := ce.True.parse(&g, true, yield, await); err != nil {
+				return j.Error("ConditionalExpression", err)
+			}
+			j.Score(g)
+			j.AcceptRunWhitespace()
+			if !j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ":"}) {
+				return j.Error("ConditionalExpression", ErrMissingColon)
+			}
+			j.AcceptRunWhitespace()
+			g = j.NewGoal()
+			ce.False = new(AssignmentExpression)
+			if err := ce.False.parse(&g, true, yield, await); err != nil {
+				return j.Error("ConditionalExpression", err)
+			}
+			j.Score(g)
 		}
-		j.Score(g)
-		j.AcceptRunWhitespace()
-		if !j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ":"}) {
-			return j.Error("ConditionalExpression", ErrMissingColon)
-		}
-		j.AcceptRunWhitespace()
-		g = j.NewGoal()
-		ce.False = new(AssignmentExpression)
-		if err := ce.False.parse(&g, true, yield, await); err != nil {
-			return j.Error("ConditionalExpression", err)
-		}
-		j.Score(g)
 	}
 	ce.Tokens = j.ToTokens()
 	return nil
