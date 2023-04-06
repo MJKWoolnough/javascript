cb72fd8ce9627a1864360ced5514c433001dd3cc
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-06 12:03:06 +0100

    while the official typescript parser accepts a comma seperated list in the index signature, it's not actually valid syntax

--- typescript.go
+++ typescript.go
@@ -636,16 +636,11 @@ func (j *jsParser) ReadIndexSignature() bool {
 	if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "["}) {
 		return false
 	}
-	for {
-		g.AcceptRunWhitespace()
-		if !g.ReadParameter() {
-			return false
-		}
-		g.AcceptRunWhitespace()
-		if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ","}) {
-			break
-		}
+	g.AcceptRunWhitespace()
+	if !g.ReadParameter() {
+		return false
 	}
+	g.AcceptRunWhitespace()
 	if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "]"}) {
 		return false
 	}
