d8b7ea951189d859be7a59c82ac1d3b6d70c4644
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-15 18:16:44 +0100

    implemented WriteModuleListItem

--- minify/print.go
+++ minify/print.go
@@ -22,6 +22,27 @@ func (w *writer) Write(p []byte) {
 
 func Print(w io.Writer, m *javascript.Module) (int64, error) {
 	wr := writer{Writer: w}
-
+	for _, mi := range m.ModuleListItems {
+		wr.WriteModuleListItem(mi)
+	}
 	return wr.count, wr.err
+}
+
+func (w *writer) WriteModuleListItem(mi javascript.ModuleItem) {
+	if mi.ExportDeclaration != nil {
+		w.WriteExportDeclaration(mi.ExportDeclaration)
+	} else if mi.ImportDeclaration != nil {
+		w.WriteImportDeclaration(mi.ImportDeclaration)
+	} else if mi.StatementListItem != nil {
+		w.WriteStatementListItem(mi.StatementListItem)
+	}
+}
+
+func (w *writer) WriteExportDeclaration(ed *javascript.ExportDeclaration) {
+}
+
+func (w *writer) WriteImportDeclaration(ed *javascript.ImportDeclaration) {
+}
+
+func (w *writer) WriteStatementListItem(si *javascript.StatementListItem) {
 }
