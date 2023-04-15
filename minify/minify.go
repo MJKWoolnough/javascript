a788322add51e4e3169cd74e00b1916d3ebd2b8c
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-15 18:00:09 +0100

    added start of Walker code that will apply minifier algos

--- minify/minify.go
+++ minify/minify.go
@@ -1,5 +1,10 @@
 package minify
 
+import (
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/javascript/walk"
+)
+
 type Minifier struct{}
 
 func New(opts ...Option) *Minifier {
@@ -8,4 +13,16 @@ func New(opts ...Option) *Minifier {
 		opt(m)
 	}
 	return m
+}
+
+type walker struct {
+	*Minifier
+}
+
+func (w *walker) Handle(t javascript.Type) error {
+	return nil
+}
+
+func (m *Minifier) Process(jm *javascript.Module) {
+	walk.Walk(jm, &walker{Minifier: m})
 }
