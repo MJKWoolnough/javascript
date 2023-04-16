8885d264609b908cd8457d58e358c0b1e3b3d3bb
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-16 15:18:32 +0100

    simplified testing of transformers

--- minify/minify_test.go
+++ minify/minify_test.go
@@ -8,99 +8,52 @@ import (
 	"vimagination.zapto.org/parser"
 )
 
-func cloneToken(tk javascript.Token) javascript.Token {
-	return javascript.Token{
+func makeToken(typ parser.TokenType, data string) *javascript.Token {
+	return &javascript.Token{
 		Token: parser.Token{
-			Type: tk.Type,
-			Data: tk.Data,
+			Type: typ,
+			Data: data,
 		},
-		Pos:     tk.Pos,
-		Line:    tk.Line,
-		LinePos: tk.LinePos,
 	}
 }
 
-func TestMinify(t *testing.T) {
+func TestTransforms(t *testing.T) {
 	for n, test := range [...]struct {
-		Options []Option
-		Input   string
-		Output  func(tk javascript.Tokens) *javascript.Module
+		Options       []Option
+		Input, Output javascript.Type
 	}{
 		{
 			[]Option{Literals()},
-			"let a = false, b = true, c = undefined;",
-			func(tk javascript.Tokens) *javascript.Module {
-				f := cloneToken(tk[6])
-				f.Data = "!1"
-				t := cloneToken(tk[13])
-				t.Data = "!0"
-				u := cloneToken(tk[20])
-				u.Data = "void 0"
-				return &javascript.Module{
-					ModuleListItems: []javascript.ModuleItem{
-						{
-							StatementListItem: &javascript.StatementListItem{
-								Declaration: &javascript.Declaration{
-									LexicalDeclaration: &javascript.LexicalDeclaration{
-										BindingList: []javascript.LexicalBinding{
-											{
-												BindingIdentifier: &tk[2],
-												Initializer: &javascript.AssignmentExpression{
-													ConditionalExpression: javascript.WrapConditional(&javascript.PrimaryExpression{
-														Literal: &f,
-														Tokens:  []javascript.Token{f},
-													}),
-													Tokens: []javascript.Token{f},
-												},
-												Tokens: tk[2:7],
-											},
-											{
-												BindingIdentifier: &tk[9],
-												Initializer: &javascript.AssignmentExpression{
-													ConditionalExpression: javascript.WrapConditional(&javascript.PrimaryExpression{
-														Literal: &t,
-														Tokens:  []javascript.Token{t},
-													}),
-													Tokens: []javascript.Token{t},
-												},
-												Tokens: tk[9:14],
-											},
-											{
-												BindingIdentifier: &tk[16],
-												Initializer: &javascript.AssignmentExpression{
-													ConditionalExpression: javascript.WrapConditional(&javascript.PrimaryExpression{
-														IdentifierReference: &u,
-														Tokens:              []javascript.Token{u},
-													}),
-													Tokens: []javascript.Token{u},
-												},
-												Tokens: tk[16:21],
-											},
-										},
-										Tokens: tk[:22],
-									},
-									Tokens: tk[:22],
-								},
-								Tokens: tk[:22],
-							},
-							Tokens: tk[:22],
-						},
-					},
-					Tokens: tk[:22],
-				}
+			&javascript.PrimaryExpression{
+				Literal: makeToken(javascript.TokenIdentifier, "false"),
+			},
+			&javascript.PrimaryExpression{
+				Literal: makeToken(javascript.TokenIdentifier, "!1"),
+			},
+		},
+		{
+			[]Option{Literals()},
+			&javascript.PrimaryExpression{
+				Literal: makeToken(javascript.TokenIdentifier, "true"),
+			},
+			&javascript.PrimaryExpression{
+				Literal: makeToken(javascript.TokenIdentifier, "!0"),
+			},
+		},
+		{
+			[]Option{Literals()},
+			&javascript.PrimaryExpression{
+				IdentifierReference: makeToken(javascript.TokenIdentifier, "undefined"),
+			},
+			&javascript.PrimaryExpression{
+				IdentifierReference: makeToken(javascript.TokenIdentifier, "void 0"),
 			},
 		},
 	} {
-		tk := parser.NewStringTokeniser(test.Input)
-		m := New(test.Options...)
-		out, err := javascript.ParseModule(&tk)
-		if err != nil {
-			t.Errorf("test %d: unexpected error parsing test input: %s", n+1, err)
-		} else {
-			m.Process(out)
-			if expected := test.Output(out.Tokens); !reflect.DeepEqual(out, expected) {
-				t.Errorf("test %d: expecting \n%+v\n...got...\n%+v", n+1, out, expected)
-			}
+		w := walker{New(test.Options...)}
+		w.Handle(test.Input)
+		if !reflect.DeepEqual(test.Input, test.Output) {
+			t.Errorf("test %d: expecting \n%+v\n...got...\n%+v", n+1, test.Output, test.Input)
 		}
 	}
 }
