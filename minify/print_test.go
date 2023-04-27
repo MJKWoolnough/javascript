8411c06d91bec96a6eb5f4e7d656ce000b9ef3d2
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-27 20:14:13 +0100

    added first print test

---
+++ minify/print_test.go
@@ -0,0 +1,33 @@
+package minify
+
+import (
+	"strings"
+	"testing"
+
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/parser"
+)
+
+func TestPrint(t *testing.T) {
+	for n, test := range [...]struct {
+		Input, Output string
+	}{
+		{
+			"var a = 1;",
+			"var a=1",
+		},
+	} {
+		tk := parser.NewStringTokeniser(test.Input)
+		m, err := javascript.ParseModule(&tk)
+		if err != nil {
+			t.Errorf("test %d: unexpected error: %s", n+1, err)
+		} else {
+			var sb strings.Builder
+			if _, err := Print(&sb, m); err != nil {
+				t.Errorf("test %d: unexpected error: %s", n+1, err)
+			} else if str := sb.String(); str != test.Output {
+				t.Errorf("test %d: expecting output %q, got %q", n+1, test.Output, str)
+			}
+		}
+	}
+}
