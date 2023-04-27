0b9a7a0360fd72f485810231e977993062ca0a23
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-27 20:47:20 +0100

    added test numbers

--- minify/print_test.go
+++ minify/print_test.go
@@ -12,51 +12,51 @@ func TestPrint(t *testing.T) {
 	for n, test := range [...]struct {
 		Input, Output string
 	}{
-		{
+		{ // 1
 			"var a = 1;",
 			"var a=1",
 		},
-		{
+		{ // 2
 			"var [a] = 1;",
 			"var[a]=1",
 		},
-		{
+		{ // 3
 			"async function a(){}",
 			"async function a(){}",
 		},
-		{
+		{ // 4
 			"typeof []",
 			"typeof[]",
 		},
-		{
+		{ // 4
 			"[] instanceof [].prototype",
 			"[]instanceof[].prototype",
 		},
-		{
+		{ // 5
 			"export * from 'a';",
 			"export*from'a'",
 		},
-		{
+		{ // 6
 			"export * as a from 'b';",
 			"export*as a from'b'",
 		},
-		{
+		{ // 7
 			"export {a, b as c, d} from 'f';",
 			"export{a,b as c,d}from'f'",
 		},
-		{
+		{ // 8
 			"import * as a from 'b';",
 			"import*as a from'b'",
 		},
-		{
+		{ // 9
 			"import {a, b as c, d} from 'e';",
 			"import{a,b as c,d}from'e'",
 		},
-		{
+		{ // 10
 			"import a from 'b';",
 			"import a from'b'",
 		},
-		{
+		{ // 11
 			"import a, {b, c} from 'e';",
 			"import a,{b,c}from'e'",
 		},
