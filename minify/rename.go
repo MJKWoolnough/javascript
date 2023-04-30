53c5cfa11c9b0983e6f59f6e401340aaaaf62567
Michael Woolnough <michael.woolnough@gmail.com>
2023-04-30 16:32:14 +0100

    globals are added to the binding list, but the NameSet flag is set true (as they're not to be renamed)

--- minify/rename.go
+++ minify/rename.go
@@ -7,13 +7,21 @@ import (
 )
 
 type binding struct {
-	Name  string
-	Scope *scope.Scope
+	Name    string
+	Scope   *scope.Scope
+	NameSet bool
 }
 
 func orderedScope(s *scope.Scope) []binding {
 	b := walkScope(s, nil)
 	sort.Slice(b, func(i, j int) bool {
+		if b[i].NameSet {
+			if !b[j].NameSet {
+				return false
+			}
+		} else if b[j].NameSet {
+			return true
+		}
 		return len(b[i].Scope.Bindings[b[i].Name]) > len(b[j].Scope.Bindings[b[j].Name])
 	})
 	return b
@@ -21,12 +29,13 @@ func orderedScope(s *scope.Scope) []binding {
 
 func walkScope(s *scope.Scope, b []binding) []binding {
 	for name := range s.Bindings {
-		if name == "this" || name == "arguments" || s.Bindings[name][0].BindingType == scope.BindingRef {
+		if name == "this" || name == "arguments" {
 			continue
 		}
 		b = append(b, binding{
-			Name:  name,
-			Scope: s,
+			Name:    name,
+			Scope:   s,
+			NameSet: s.Bindings[name][0].BindingType == scope.BindingRef,
 		})
 	}
 	for _, cs := range s.Scopes {
