aa2cef2d4f89b8ee6e1513be80758dc757c4b1fb
Michael Woolnough <michael.woolnough@gmail.com>
2023-05-23 20:23:18 +0100

    switched to numeric options, not fns

--- minify/options.go
+++ minify/options.go
@@ -1,51 +1,18 @@
 package minify
 
-type Option func(*minifier)
+type Option uint64
 
-func Literals(m *minifier) {
-	m.literals = true
-}
-
-func ArrowFn(m *minifier) {
-	m.arrowFn = true
-}
-
-func IfToConditional(m *minifier) {
-	m.ifToConditional = true
-}
-
-func RemoveDebugger(m *minifier) {
-	m.rmDebugger = true
-}
-
-func RenameIdentifiers(m *minifier) {
-	m.rename = true
-}
-
-func BlocksToStatement(m *minifier) {
-	m.blocks = true
-}
-
-func Keys(m *minifier) {
-	m.keys = true
-}
-
-func RemoveExpressionNames(m *minifier) {
-	m.nonHoistableNames = true
-}
-
-func FunctionExpressionToArrowFunc(m *minifier) {
-	m.replaceFEWithAF = true
-}
-
-func UnwrapParens(m *minifier) {
-	m.unwrapParens = true
-}
-
-func RemoveLastEmptyReturn(m *minifier) {
-	m.removeLastReturn = true
-}
-
-func CombineExpressionRuns(m *minifier) {
-	m.combineExpressions = true
-}
+const (
+	Literals = 1 << iota
+	ArrowFn
+	IfToConditional
+	RemoveDebugger
+	RenameIdentifiers
+	BlocksToStatement
+	Keys
+	RemoveExpressionNames
+	FunctionExpressionToArrowFunc
+	UnwrapParens
+	RemoveLastEmptyReturn
+	CombineExpressionRuns
+)
