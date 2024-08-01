89d69258eab96585fd6e1c5054ee0551bc68cd50
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-01 21:59:19 +0100

    reformatted

--- ast_class.go
+++ ast_class.go
@@ -17,7 +17,9 @@ func (cd *ClassDeclaration) parse(j *jsParser, yield, await, def bool) error {
 	if !j.AcceptToken(parser.Token{Type: TokenKeyword, Data: "class"}) {
 		return j.Error("ClassDeclaration", ErrInvalidClassDeclaration)
 	}
+
 	j.AcceptRunWhitespace()
+
 	if cd.BindingIdentifier = j.parseIdentifier(yield, await); cd.BindingIdentifier == nil {
 		if !def {
 			return j.Error("ClassDeclaration", ErrNoIdentifier)
@@ -25,58 +27,77 @@ func (cd *ClassDeclaration) parse(j *jsParser, yield, await, def bool) error {
 	} else {
 		j.AcceptRunWhitespace()
 	}
+
 	if j.SkipGeneric() {
 		j.AcceptRunWhitespace()
 	}
+
 	if j.SkipHeritage() {
 		j.AcceptRunWhitespace()
 	}
+
 	if j.AcceptToken(parser.Token{Type: TokenKeyword, Data: "extends"}) {
 		j.AcceptRunWhitespace()
+
 		g := j.NewGoal()
+
 		cd.ClassHeritage = new(LeftHandSideExpression)
 		if err := cd.ClassHeritage.parse(&g, yield, await); err != nil {
 			return j.Error("ClassDeclaration", err)
 		}
+
 		j.Score(g)
 		j.AcceptRunWhitespace()
+
 		if j.SkipTypeArguments() {
 			j.AcceptRunWhitespace()
 		}
 	}
+
 	if j.SkipHeritage() {
 		j.AcceptRunWhitespace()
 	}
+
 	if !j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "{"}) {
 		return j.Error("ClassDeclaration", ErrMissingOpeningBrace)
 	}
+
 	for {
 		j.AcceptRunWhitespace()
+
 		if j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ";"}) {
 			continue
 		} else if j.Accept(TokenRightBracePunctuator) {
 			break
 		}
+
 		g := j.NewGoal()
 		if g.SkipParameterProperties() {
 			g.AcceptRunWhitespace()
 		}
+
 		if g.SkipAbstractField() {
 			j.Score(g)
 			continue
 		}
+
 		if g.SkipIndexSignature() {
 			j.Score(g)
 			continue
 		}
+
 		md := len(cd.ClassBody)
+
 		cd.ClassBody = append(cd.ClassBody, ClassElement{})
 		if err := cd.ClassBody[md].parse(&g, yield, await); err != nil {
 			return j.Error("ClassDeclaration", err)
 		}
+
 		j.Score(g)
 	}
+
 	cd.Tokens = j.ToTokens()
+
 	return nil
 }
 
@@ -98,59 +119,76 @@ type ClassElement struct {
 func (ce *ClassElement) parse(j *jsParser, yield, await bool) error {
 	if j.Peek() == (parser.Token{Type: TokenIdentifier, Data: "static"}) {
 		g := j.NewGoal()
+
 		g.Skip()
 		g.AcceptRunWhitespace()
+
 		if tk := g.Peek(); (tk.Type != TokenPunctuator || (tk.Data != "=" && tk.Data != ";" && tk.Data != "(" && tk.Data != ":")) && tk.Type != TokenRightBracePunctuator {
 			ce.Static = true
+
 			j.Skip()
 			j.AcceptRunWhitespace()
 		}
 	}
+
 	j.SkipReadOnly()
+
 	if ce.Static && j.Peek() == (parser.Token{Type: TokenPunctuator, Data: "{"}) {
-		ce.ClassStaticBlock = new(Block)
 		g := j.NewGoal()
+
+		ce.ClassStaticBlock = new(Block)
 		if err := ce.ClassStaticBlock.parse(&g, false, true, false); err != nil {
 			return j.Error("ClassElement", err)
 		}
+
 		j.Score(g)
 	} else {
 		g := j.NewGoal()
 		h := g.NewGoal()
+
 		var (
 			cen      ClassElementName
 			isMethod bool
 		)
+
 		if h.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "*"}) {
 			isMethod = true
 		} else if h.AcceptToken(parser.Token{Type: TokenIdentifier, Data: "async"}) {
 			h.AcceptRunWhitespaceNoNewLine()
+
 			tk := h.Peek()
+
 			isMethod = tk == (parser.Token{Type: TokenPunctuator, Data: "*"}) || tk == (parser.Token{Type: TokenPunctuator, Data: "["}) || tk == (parser.Token{Type: TokenPunctuator, Data: "("}) || tk.Type == TokenIdentifier || tk.Type == TokenPrivateIdentifier
 			if !isMethod && h.SkipGeneric() {
 				h.AcceptRunWhitespace()
+
 				tk := h.Peek()
 				isMethod = tk == (parser.Token{Type: TokenPunctuator, Data: "("})
 			}
 		} else if h.AcceptToken(parser.Token{Type: TokenIdentifier, Data: "get"}) || h.AcceptToken(parser.Token{Type: TokenIdentifier, Data: "set"}) {
 			h.AcceptRunWhitespace()
+
 			tk := h.Peek()
 			isMethod = tk == (parser.Token{Type: TokenPunctuator, Data: "["}) || tk == (parser.Token{Type: TokenPunctuator, Data: "("}) || tk.Type == TokenIdentifier || tk.Type == TokenPrivateIdentifier
 		} else {
 			if err := cen.parse(&g, yield, await); err != nil {
 				return j.Error("ClassElement", err)
 			}
+
 			h = g.NewGoal()
+
 			h.AcceptRunWhitespace()
+
 			if h.SkipGeneric() {
 				h.AcceptRunWhitespace()
 			}
+
 			isMethod = h.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "("})
 		}
+
 		if isMethod {
-			ce.MethodDefinition = &MethodDefinition{
-				ClassElementName: cen,
-			}
+			ce.MethodDefinition = &MethodDefinition{ClassElementName: cen}
+
 			for {
 				h := g.NewGoal()
 				if h.SkipMethodOverload(ce.Static, cen, yield, await) {
@@ -159,23 +197,26 @@ func (ce *ClassElement) parse(j *jsParser, yield, await bool) error {
 					break
 				}
 			}
+
 			if err := ce.MethodDefinition.parse(&g, yield, await); err != nil {
 				return j.Error("ClassElement", err)
 			}
 		} else {
-			ce.FieldDefinition = &FieldDefinition{
-				ClassElementName: cen,
-			}
+			ce.FieldDefinition = &FieldDefinition{ClassElementName: cen}
 			if err := ce.FieldDefinition.parse(&g, yield, await); err != nil {
 				return j.Error("ClassElement", err)
 			}
+
 			if g.GetLastToken().Token != (parser.Token{Type: TokenPunctuator, Data: ";"}) {
 				h := g.NewGoal()
+
 				h.AcceptRunWhitespaceNoNewLine()
+
 				if h.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ";"}) {
 					g.Score(h)
 				} else if h.Accept(TokenLineTerminator, TokenSingleLineComment, TokenMultiLineComment) {
 					h.AcceptRunWhitespace()
+
 					if h.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ";"}) {
 						g.Score(h)
 					}
@@ -184,9 +225,12 @@ func (ce *ClassElement) parse(j *jsParser, yield, await bool) error {
 				}
 			}
 		}
+
 		j.Score(g)
 	}
+
 	ce.Tokens = j.ToTokens()
+
 	return nil
 }
 
@@ -205,13 +249,17 @@ func (cen *ClassElementName) parse(j *jsParser, yield, await bool) error {
 		cen.PrivateIdentifier = j.GetLastToken()
 	} else {
 		g := j.NewGoal()
+
 		cen.PropertyName = new(PropertyName)
 		if err := cen.PropertyName.parse(&g, yield, await); err != nil {
 			return j.Error("ClassElementName", err)
 		}
+
 		j.Score(g)
 	}
+
 	cen.Tokens = j.ToTokens()
+
 	return nil
 }
 
@@ -227,33 +275,48 @@ func (fd *FieldDefinition) parse(j *jsParser, yield, await bool) error {
 	if len(fd.ClassElementName.Tokens) == 0 {
 		fd.ClassElementName.parse(j, yield, await)
 	}
+
 	g := j.NewGoal()
+
 	g.AcceptRunWhitespace()
+
 	if g.SkipForce() {
 		h := g.NewGoal()
+
 		h.AcceptRunWhitespace()
+
 		if h.SkipColonType() {
 			g.Score(h)
 		}
+
 		j.Score(g)
+
 		g = j.NewGoal()
+
 		g.AcceptRunWhitespace()
 	} else if g.SkipOptionalColonType() {
 		j.Score(g)
+
 		g = j.NewGoal()
+
 		g.AcceptRunWhitespace()
 	}
 	if g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "="}) {
 		g.AcceptRunWhitespace()
-		fd.Initializer = new(AssignmentExpression)
+
 		h := g.NewGoal()
+
+		fd.Initializer = new(AssignmentExpression)
 		if err := fd.Initializer.parse(&h, true, yield, await); err != nil {
 			return g.Error("FieldDefinition", err)
 		}
+
 		g.Score(h)
 		j.Score(g)
 	}
+
 	fd.Tokens = j.ToTokens()
+
 	return nil
 }
 
@@ -282,100 +345,138 @@ type MethodDefinition struct {
 
 func (md *MethodDefinition) parse(j *jsParser, yield, await bool) error {
 	var prev MethodType
+
 	if len(md.ClassElementName.Tokens) == 0 {
 		g := j.NewGoal()
 		switch g.Peek() {
 		case parser.Token{Type: TokenIdentifier, Data: "get"}:
 			g.Skip()
 			g.AcceptRunWhitespace()
+
 			md.Type = MethodGetter
 		case parser.Token{Type: TokenIdentifier, Data: "set"}:
 			g.Skip()
 			g.AcceptRunWhitespace()
+
 			md.Type = MethodSetter
 		case parser.Token{Type: TokenIdentifier, Data: "async"}:
 			g.Skip()
+
 			if t := g.AcceptRunWhitespaceNoNewLine(); t == TokenLineTerminator || t == TokenSingleLineComment || t == TokenMultiLineComment || g.SkipGeneric() {
 				g = j.NewGoal()
+
 				break
 			}
+
 			md.Type = MethodAsync
+
 			fallthrough
 		default:
 			if g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "*"}) {
 				j.Score(g)
 				j.AcceptRunWhitespace()
+
 				g = j.NewGoal()
+
 				if md.Type == MethodAsync {
 					md.Type = MethodAsyncGenerator
 				} else {
 					md.Type = MethodGenerator
 				}
+
 				prev = md.Type
 			}
 		}
+
 		if g.Peek() == (parser.Token{Type: TokenPunctuator, Data: "("}) {
 			md.Type = prev
 		} else {
 			j.Score(g)
 		}
+
 		g = j.NewGoal()
+
 		if err := md.ClassElementName.parse(&g, yield, await); err != nil {
 			return j.Error("MethodDefinition", err)
 		}
+
 		j.Score(g)
 	}
+
 	j.AcceptRunWhitespace()
+
 	if j.SkipGeneric() {
 		j.AcceptRunWhitespace()
 	}
+
 	switch md.Type {
 	case MethodGetter:
 		g := j.NewGoal()
+
 		if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "("}) {
 			return g.Error("MethodDefinition", ErrMissingOpeningParenthesis)
 		}
+
 		g.AcceptRunWhitespace()
+
 		if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ")"}) {
 			return g.Error("MethodDefinition", ErrMissingClosingParenthesis)
 		}
+
 		md.Params.Tokens = g.ToTokens()
+
 		j.Score(g)
 	case MethodSetter:
 		g := j.NewGoal()
+
 		if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "("}) {
 			return g.Error("MethodDefinition", ErrMissingOpeningParenthesis)
 		}
+
 		g.AcceptRunWhitespace()
+
 		md.Params.FormalParameterList = make([]BindingElement, 1)
 		h := g.NewGoal()
+
 		if err := md.Params.FormalParameterList[0].parse(&h, nil, false, false); err != nil {
 			return j.Error("MethodDefinition", err)
 		}
+
 		g.Score(h)
 		g.AcceptRunWhitespace()
+
 		if !g.AcceptToken(parser.Token{Type: TokenPunctuator, Data: ")"}) {
 			return g.Error("MethodDefinition", ErrMissingClosingParenthesis)
 		}
+
 		md.Params.Tokens = g.ToTokens()
+
 		j.Score(g)
 	default:
 		g := j.NewGoal()
+
 		if err := md.Params.parse(&g, md.Type == MethodGenerator, md.Type == MethodAsync); err != nil {
 			return j.Error("MethodDefinition", err)
 		}
+
 		j.Score(g)
 	}
+
 	j.AcceptRunWhitespace()
+
 	if j.SkipReturnType() {
 		j.AcceptRunWhitespace()
 	}
+
 	g := j.NewGoal()
 	if err := md.FunctionBody.parse(&g, md.Type == MethodGenerator, md.Type == MethodAsync, true); err != nil {
 		return j.Error("MethodDefinition", err)
 	}
+
 	j.Score(g)
+
 	md.Tokens = j.ToTokens()
+
 	return nil
 }
 
@@ -392,13 +493,17 @@ type PropertyName struct {
 func (pn *PropertyName) parse(j *jsParser, yield, await bool) error {
 	if j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "["}) {
 		j.AcceptRunWhitespace()
+
 		g := j.NewGoal()
+
 		pn.ComputedPropertyName = new(AssignmentExpression)
 		if err := pn.ComputedPropertyName.parse(&g, true, yield, await); err != nil {
 			return j.Error("PropertyName", err)
 		}
+
 		j.Score(g)
 		j.AcceptRunWhitespace()
+
 		if !j.AcceptToken(parser.Token{Type: TokenPunctuator, Data: "]"}) {
 			return j.Error("PropertyName", ErrMissingClosingBracket)
 		}
@@ -407,6 +512,8 @@ func (pn *PropertyName) parse(j *jsParser, yield, await bool) error {
 	} else {
 		return j.Error("PropertyName", ErrInvalidPropertyName)
 	}
+
 	pn.Tokens = j.ToTokens()
+
 	return nil
 }
