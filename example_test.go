65f9402c9b9fdda6d76be8f3072b6f18094f7462
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-18 19:17:42 +0100

    added example

---
+++ example_test.go
@@ -0,0 +1,32 @@
+package javascript_test
+
+import (
+	"fmt"
+
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/parser"
+)
+
+func Example() {
+	src := `function greet(name) {console.log("Hello, " + name)} for (const name of ["Alice", "Bob", "Charlie"]) greet(name)`
+
+	tk := parser.NewStringTokeniser(src)
+
+	ast, err := javascript.ParseScript(&tk)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	javascript.UnwrapConditional(javascript.WrapConditional(javascript.UnwrapConditional(javascript.UnwrapConditional(ast.StatementList[0].Declaration.FunctionDeclaration.FunctionBody.StatementList[0].Statement.ExpressionStatement.Expressions[0].ConditionalExpression).(*javascript.CallExpression).Arguments.ArgumentList[0].AssignmentExpression.ConditionalExpression).(*javascript.AdditiveExpression).AdditiveExpression)).(*javascript.PrimaryExpression).Literal.Data = `"Hi, "`
+
+	fmt.Printf("%s", ast)
+
+	// Output:
+	// function greet(name) {
+	//	console.log("Hi, " + name);
+	// }
+	//
+	// for (const name of ["Alice", "Bob", "Charlie"]) greet(name);
+}
