9888519ba55fa1c3a40dffd264f06d08dea0f55b
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-24 08:51:08 +0100

    regen'd

--- format_types.go
+++ format_types.go
@@ -1253,6 +1253,17 @@ func (f *ImportSpecifier) printType(w writer, v bool) {
 		pp.WriteString("\nImportedBinding: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
