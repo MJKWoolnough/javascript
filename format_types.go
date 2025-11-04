b4963673b758369b181a93784e8592e7027f1e93
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-04 07:09:11 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -2079,6 +2079,17 @@ func (f *ParenthesizedExpression) printType(w writer, v bool) {
 		pp.WriteString("\nExpressions: []")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
