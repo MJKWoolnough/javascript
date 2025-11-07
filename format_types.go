ba650574660776c827167c9ae1a7c05605fee3c6
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-07 19:27:08 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -451,6 +451,17 @@ func (f *BindingProperty) printType(w writer, v bool) {
 	pp.WriteString("\nBindingElement: ")
 	f.BindingElement.printType(pp, v)
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
