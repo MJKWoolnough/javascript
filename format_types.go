55b76bb420cfcfd4a3a91d9f0d0a914aea7f4f49
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-07 18:49:57 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -423,6 +423,17 @@ func (f *BindingElement) printType(w writer, v bool) {
 		pp.WriteString("\nInitializer: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
