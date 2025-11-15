e0850e7198646ad2c4412f6ab2e5c4f32e4e003e
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-15 14:32:09 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -2175,6 +2175,17 @@ func (f *OptionalChain) printType(w writer, v bool) {
 		pp.WriteString("\nPrivateIdentifier: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
