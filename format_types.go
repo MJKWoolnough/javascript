6b96bd26195e2e317b8a02c294c0e4abeae35c56
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-23 13:39:23 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -406,6 +406,17 @@ func (f *AssignmentProperty) printType(w writer, v bool) {
 		pp.WriteString("\nInitializer: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
