77551b5ae6981f4c28d17af6009816e7134c67dd
Michael Woolnough <michael.woolnough@gmail.com>
2025-11-24 18:45:16 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -275,6 +275,17 @@ func (f *ArrowFunction) printType(w writer, v bool) {
 		pp.WriteString("\nFunctionBody: nil")
 	}
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
