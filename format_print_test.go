e18eb81b4ef5af2641e1c597d31309df87b07aa6
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-03 07:38:00 +0000

    added whitespace printing tests for BitwiseXORExpression

--- format_print_test.go
+++ format_print_test.go
@@ -2454,6 +2454,16 @@ func TestPrintingScript(t *testing.T) {
 			"a | b;",
 			"a // A\n| // B\nb;",
 		},
+		{ // 487
+			"a /* A */ ^ /* B */ b",
+			"a ^ b;",
+			"a /* A */ ^ /* B */ b;",
+		},
+		{ // 488
+			"a // A\n^ // B\nb",
+			"a ^ b;",
+			"a // A\n^ // B\nb;",
+		},
 	} {
 		for m, in := range [2]string{test.Input, test.VerboseOutput} {
 			s, err := ParseScript(makeTokeniser(parser.NewStringTokeniser(in)))
