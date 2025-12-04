88e4d60eadad3f3ff8ea49d8e002ca5ca91c0240
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-04 07:51:53 +0000

    added whitespace printing tests for MultiplicativeExpression

--- format_print_test.go
+++ format_print_test.go
@@ -2514,6 +2514,16 @@ func TestPrintingScript(t *testing.T) {
 			"a - b;",
 			"a // A\n- // B\nb;",
 		},
+		{ // 497
+			"a /* A */ * /* B */ b",
+			"a * b;",
+			"a /* A */ * /* B */ b;",
+		},
+		{ // 498
+			"a // A\n% // B\nb",
+			"a % b;",
+			"a // A\n% // B\nb;",
+		},
 	} {
 		for m, in := range [2]string{test.Input, test.VerboseOutput} {
 			s, err := ParseScript(makeTokeniser(parser.NewStringTokeniser(in)))
