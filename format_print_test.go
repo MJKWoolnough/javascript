fcd361a4269684d36cfa69a5cd77a8315d532573
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-05 08:44:35 +0000

    added whitespace printing tests for ExponentiationExpression

--- format_print_test.go
+++ format_print_test.go
@@ -2524,6 +2524,16 @@ func TestPrintingScript(t *testing.T) {
 			"a % b;",
 			"a // A\n% // B\nb;",
 		},
+		{ // 499
+			"a /* A */ ** /* B */ b",
+			"a ** b;",
+			"a /* A */ ** /* B */ b;",
+		},
+		{ // 500
+			"a // A\n** // B\nb",
+			"a ** b;",
+			"a // A\n** // B\nb;",
+		},
 	} {
 		for m, in := range [2]string{test.Input, test.VerboseOutput} {
 			s, err := ParseScript(makeTokeniser(parser.NewStringTokeniser(in)))
