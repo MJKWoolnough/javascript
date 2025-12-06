77b8238a051dab57010ab9bbbce5b181b5f18d86
Michael Woolnough <michael.woolnough@gmail.com>
2025-12-06 16:45:05 +0000

    regen'd

--- format_types.go
+++ format_types.go
@@ -2600,6 +2600,17 @@ func (f *RelationalExpression) printType(w writer, v bool) {
 	pp.WriteString("\nShiftExpression: ")
 	f.ShiftExpression.printType(pp, v)
 
+	pp.WriteString("\nComments: [")
+
+	ipp := pp.Indent()
+
+	for n, e := range f.Comments {
+		ipp.Printf("\n%d: ", n)
+		e.printType(ipp, v)
+	}
+
+	pp.WriteString("\n]")
+
 	pp.WriteString("\nTokens: ")
 	f.Tokens.printType(pp, v)
 
