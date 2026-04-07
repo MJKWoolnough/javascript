f16053e6c5bab689eb7006a5a8b815ca89d944c7
Michael Woolnough <michael.woolnough@gmail.com>
2026-04-07 10:51:12 +0100

    added processLeftHandSideExpression error handling test

--- scope/scope_test.go
+++ scope/scope_test.go
@@ -4576,6 +4576,15 @@ func TestModuleScope(t *testing.T) {
 				}
 			},
 		},
+		{ // 79
+			`a?.b[() => {let c,c}]`,
+			func(m *javascript.Module) (*Scope, error) {
+				return nil, ErrDuplicateDeclaration{
+					Declaration: javascript.UnwrapConditional(m.ModuleListItems[0].StatementListItem.Statement.ExpressionStatement.Expressions[0].ConditionalExpression).(*javascript.OptionalExpression).OptionalChain.Expression.Expressions[0].ArrowFunction.FunctionBody.StatementList[0].Declaration.LexicalDeclaration.BindingList[0].BindingIdentifier,
+					Duplicate:   javascript.UnwrapConditional(m.ModuleListItems[0].StatementListItem.Statement.ExpressionStatement.Expressions[0].ConditionalExpression).(*javascript.OptionalExpression).OptionalChain.Expression.Expressions[0].ArrowFunction.FunctionBody.StatementList[0].Declaration.LexicalDeclaration.BindingList[1].BindingIdentifier,
+				}
+			},
+		},
 	} {
 		tk := parser.NewStringTokeniser(test.Input)
 
