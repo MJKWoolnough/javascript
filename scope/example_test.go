91aa53f680903ece607dba3666bda0ce788c4c8c
Michael Woolnough <michael.woolnough@gmail.com>
2026-04-11 21:32:31 +0100

    added example

---
+++ scope/example_test.go
@@ -0,0 +1,38 @@
+package scope_test
+
+import (
+	"fmt"
+
+	"vimagination.zapto.org/javascript"
+	"vimagination.zapto.org/javascript/scope"
+	"vimagination.zapto.org/parser"
+)
+
+func Example() {
+	src := `let a = 1; console.log(a)`
+
+	tk := parser.NewStringTokeniser(src)
+
+	ast, err := javascript.ParseModule(&tk)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	s, err := scope.ModuleScope(ast, nil)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	s.Rename("a", "b")
+
+	fmt.Printf("%s", ast)
+
+	// Output:
+	// let b = 1;
+	//
+	// console.log(b);
+}
