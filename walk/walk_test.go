7211d7a3dca68910056190c73ebee58c18d03894
Michael Woolnough <michael.woolnough@gmail.com>
2026-04-13 07:47:44 +0100

    added test numbers

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -37,254 +37,254 @@ func TestWalk(t *testing.T) {
 		End   func(m *javascript.Module) javascript.Type
 		Level []string
 	}{
-		{
+		{ // 1
 			"",
 			func(m *javascript.Module) javascript.Type { return nil },
 			nil,
 		},
-		{
+		{ // 2
 			"import a from './b';",
 			func(m *javascript.Module) javascript.Type { return &m.ModuleListItems[0] },
 			[]string{"Module", "ModuleItem"},
 		},
-		{
+		{ // 3
 			"import a from './b';",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ImportDeclaration },
 			[]string{"Module", "ModuleItem", "ImportDeclaration"},
 		},
-		{
+		{ // 4
 			"import a from './b';",
 			func(m *javascript.Module) javascript.Type { return nil },
 			nil,
 		},
-		{
+		{ // 5
 			"import a from './b';",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ImportDeclaration.ImportClause },
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "ImportClause"},
 		},
-		{
+		{ // 6
 			"import a from './b';",
 			func(m *javascript.Module) javascript.Type { return &m.ModuleListItems[0].ImportDeclaration.FromClause },
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "FromClause"},
 		},
-		{
+		{ // 7
 			"import a from './b' with {};",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ImportDeclaration.WithClause },
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "WithClause"},
 		},
-		{
+		{ // 8
 			"import {} from './b';",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ImportDeclaration.ImportClause.NamedImports
 			},
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "ImportClause", "NamedImports"},
 		},
-		{
+		{ // 9
 			"import {} from './b';",
 			func(m *javascript.Module) javascript.Type { return nil },
 			nil,
 		},
-		{
+		{ // 10
 			"import {a} from './b';",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].ImportDeclaration.ImportClause.NamedImports.ImportList[0]
 			},
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "ImportClause", "NamedImports", "ImportSpecifier"},
 		},
-		{
+		{ // 11
 			"import {a, b} from './c';",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].ImportDeclaration.ImportClause.NamedImports.ImportList[1]
 			},
 			[]string{"Module", "ModuleItem", "ImportDeclaration", "ImportClause", "NamedImports", "ImportSpecifier"},
 		},
-		{
+		{ // 12
 			"export {};",
 			func(m *javascript.Module) javascript.Type { return nil },
 			nil,
 		},
-		{
+		{ // 13
 			"export default a;",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ExportDeclaration },
 			[]string{"Module", "ModuleItem", "ExportDeclaration"},
 		},
-		{
+		{ // 14
 			"export {a};",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ExportDeclaration.ExportClause },
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "ExportClause"},
 		},
-		{
+		{ // 15
 			"export {a} from './b';",
 			func(m *javascript.Module) javascript.Type { return m.ModuleListItems[0].ExportDeclaration.FromClause },
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "FromClause"},
 		},
-		{
+		{ // 16
 			"export var a;",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ExportDeclaration.VariableStatement
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "VariableStatement"},
 		},
-		{
+		{ // 17
 			"export let a;",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ExportDeclaration.Declaration
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "Declaration"},
 		},
-		{
+		{ // 18
 			"export default function a(){}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ExportDeclaration.DefaultFunction
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "FunctionDeclaration"},
 		},
-		{
+		{ // 19
 			"export default class a{}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ExportDeclaration.DefaultClass
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "ClassDeclaration"},
 		},
-		{
+		{ // 20
 			"export default () => {}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].ExportDeclaration.DefaultAssignmentExpression
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "AssignmentExpression"},
 		},
-		{
+		{ // 21
 			"export {a, b}",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].ExportDeclaration.ExportClause.ExportList[0]
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "ExportClause", "ExportSpecifier"},
 		},
-		{
+		{ // 22
 			"export {a, b}",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].ExportDeclaration.ExportClause.ExportList[1]
 			},
 			[]string{"Module", "ModuleItem", "ExportDeclaration", "ExportClause", "ExportSpecifier"},
 		},
-		{
+		{ // 23
 			"a",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem"},
 		},
-		{
+		{ // 24
 			"a",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement"},
 		},
-		{
+		{ // 25
 			"let a",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Declaration
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Declaration"},
 		},
-		{
+		{ // 26
 			"{}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.BlockStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "Block"},
 		},
-		{
+		{ // 27
 			"var a",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.VariableStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "VariableStatement"},
 		},
-		{
+		{ // 28
 			"a",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.ExpressionStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "Expression"},
 		},
-		{
+		{ // 29
 			"if (a) {}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.IfStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "IfStatement"},
 		},
-		{
+		{ // 30
 			"do; while (a)",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.IterationStatementDo
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "IterationStatementDo"},
 		},
-		{
+		{ // 31
 			"while (a){}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.IterationStatementWhile
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "IterationStatementWhile"},
 		},
-		{
+		{ // 32
 			"for (;;) {}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.IterationStatementFor
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "IterationStatementFor"},
 		},
-		{
+		{ // 33
 			"switch (a){}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.SwitchStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "SwitchStatement"},
 		},
-		{
+		{ // 34
 			"with (a){}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.WithStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "WithStatement"},
 		},
-		{
+		{ // 35
 			"a: function b (){}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.LabelledItemFunction
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "FunctionDeclaration"},
 		},
-		{
+		{ // 36
 			"a: b",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.LabelledItemStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "Statement"},
 		},
-		{
+		{ // 37
 			"try {} finally{}",
 			func(m *javascript.Module) javascript.Type {
 				return m.ModuleListItems[0].StatementListItem.Statement.TryStatement
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "TryStatement"},
 		},
-		{
+		{ // 38
 			"{}",
 			func(m *javascript.Module) javascript.Type { return nil },
 			nil,
 		},
-		{
+		{ // 39
 			"{a}",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].StatementListItem.Statement.BlockStatement.StatementList[0]
 			},
 			[]string{"Module", "ModuleItem", "StatementListItem", "Statement", "Block", "StatementListItem"},
 		},
-		{
+		{ // 40
 			"{a; b}",
 			func(m *javascript.Module) javascript.Type {
 				return &m.ModuleListItems[0].StatementListItem.Statement.BlockStatement.StatementList[1]
