0099618afba5796c88668103c75bc5e9810135f2
Michael Woolnough <michael.woolnough@gmail.com>
2026-04-30 05:48:34 +0100

    generic arrow funcs are illegal in TSX mode (https://www.typescriptlang.org/docs/handbook/jsx.html#the-as-operator)

--- tokeniser.go
+++ tokeniser.go
@@ -11,9 +11,8 @@ import (
 )
 
 const (
-	whitespace         = "\t\v\f \u00a0\ufeff" // Tab, Vertical Tab, Form Feed, Space, No-break space, ZeroWidth No-Break Space, https://262.ecma-international.org/11.0/#table-32
-	lineTerminators    = "\n\r\u2028\u2029"    // Line Feed, Carriage Return, Line Separator, Paragraph Separator, https://262.ecma-international.org/11.0/#table-33
-	combinedWhitespace = whitespace + lineTerminators
+	whitespace      = "\t\v\f \u00a0\ufeff" // Tab, Vertical Tab, Form Feed, Space, No-break space, ZeroWidth No-Break Space, https://262.ecma-international.org/11.0/#table-32
+	lineTerminators = "\n\r\u2028\u2029"    // Line Feed, Carriage Return, Line Separator, Paragraph Separator, https://262.ecma-international.org/11.0/#table-33
 
 	singleEscapeChar = "'\"\\bfnrtv"
 	binaryDigit      = "01"
@@ -274,11 +273,9 @@ func (j *jsTokeniser) inputElement(t *parser.Tokeniser) (parser.Token, parser.To
 			}
 		case '<':
 			if !allowDivision && j.isJSX {
-				if !j.isTypescript || checkForJSX(t) {
-					j.state = append(j.state, 'j', 'X')
+				j.state = append(j.state, 'j', 'X')
 
-					return t.Return(TokenJSXElementStart, j.jsxElement)
-				}
+				return t.Return(TokenJSXElementStart, j.jsxElement)
 			}
 
 			fallthrough
@@ -835,81 +832,5 @@ func (j *jsTokeniser) jsxChildren(t *parser.Tokeniser) (parser.Token, parser.Tok
 		return t.ReturnError(io.ErrUnexpectedEOF)
 	default:
 		return t.ReturnError(ErrInvalidCharacter)
-	}
-}
-
-func checkForJSX(t *parser.Tokeniser) bool {
-	startState := t.State()
-	defer startState.Reset()
-
-	if t.Accept(combinedWhitespace) || t.Accept("/") {
-		return false
-	}
-
-	if t.Accept(">") {
-		return true
-	}
-
-	state := t.State()
-
-	if t.AcceptString("const ", false) == 6 {
-		if t.AcceptRun(combinedWhitespace) == '>' {
-			return true
-		}
-
-		if t.AcceptRun(combinedWhitespace) == '/' {
-			t.Next()
-
-			return !t.Accept("/*")
-		}
-	} else {
-		state.Reset()
-	}
-
-	if !internal.IsIDStart(t.Next()) {
-		return false
-	}
-
-	for internal.IsIDContinue(t.Peek()) {
-		t.Next()
-	}
-
-	if t.Accept(":.-") {
-		return true
-	}
-
-	if t.AcceptRun(combinedWhitespace) == '/' {
-		t.Next()
-
-		return !t.Accept("/*")
-	}
-
-	if t.AcceptString("extends ", false) == 8 || t.Accept("=,") {
-		return false
-	}
-
-	if !t.Accept(">") {
-		return true
-	}
-
-	switch t.AcceptRun(combinedWhitespace) {
-	case '(':
-		return false // Assume ArrowFunc
-	case '/':
-		t.Next()
-
-		var c rune
-
-		if t.Accept("/") {
-			c = t.ExceptRun(lineTerminators + "{<")
-		} else if !t.Accept("*") {
-			return true
-		} else {
-			c = t.ExceptRun("*" + "{<")
-		}
-
-		return c == '{' || c == '<' // Assume JSX
-	default:
-		return true
 	}
 }
