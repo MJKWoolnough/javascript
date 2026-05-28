a062ae02f4c4299a099b7e2effa6b6e6c42cee84
Michael Woolnough <michael.woolnough@gmail.com>
2026-05-28 15:22:59 +0100

    un-split properties where possible

--- minify/print.go
+++ minify/print.go
@@ -50,6 +50,28 @@ func Print(w io.Writer, m *javascript.Module) (int64, error) {
 	var h walk.Handler
 
 	h = walk.HandlerFunc(func(t javascript.Type) error {
+		switch t := t.(type) {
+		case *javascript.PropertyDefinition:
+			if !t.IsCoverInitializedName && t.PropertyName != nil && t.PropertyName.LiteralPropertyName != nil && t.AssignmentExpression != nil && t.AssignmentExpression.ConditionalExpression != nil {
+				c := javascript.UnwrapConditional(t.AssignmentExpression.ConditionalExpression)
+
+				if pe, ok := c.(*javascript.PrimaryExpression); ok && pe.IdentifierReference != nil && pe.IdentifierReference.Type == t.PropertyName.LiteralPropertyName.Type && pe.IdentifierReference.Data == t.PropertyName.LiteralPropertyName.Data {
+					v := *t.PropertyName.LiteralPropertyName
+					pe.IdentifierReference = &v
+				}
+			}
+		case *javascript.AssignmentProperty:
+			if t.PropertyName.LiteralPropertyName != nil && t.DestructuringAssignmentTarget != nil && t.DestructuringAssignmentTarget.LeftHandSideExpression != nil && t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression != nil && len(t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression.News) == 0 && t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression.MemberExpression.PrimaryExpression != nil && t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression.MemberExpression.PrimaryExpression.IdentifierReference != nil && t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression.MemberExpression.PrimaryExpression.IdentifierReference.Data == t.PropertyName.LiteralPropertyName.Data {
+				v := *t.PropertyName.LiteralPropertyName
+				t.DestructuringAssignmentTarget.LeftHandSideExpression.NewExpression.MemberExpression.PrimaryExpression.IdentifierReference = &v
+			}
+		case *javascript.BindingProperty:
+			if t.PropertyName.LiteralPropertyName != nil && t.BindingElement.SingleNameBinding != nil && t.PropertyName.LiteralPropertyName.Data == t.BindingElement.SingleNameBinding.Data {
+				v := *t.PropertyName.LiteralPropertyName
+				t.BindingElement.SingleNameBinding = &v
+			}
+		}
+
 		v := reflect.ValueOf(t)
 
 		if v.Type().Kind() != reflect.Pointer || v.Type().Elem().Kind() != reflect.Struct {
